6b575a7bfeadaa2b1f81e7a9bd80d0f2b966f845
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2019-12-17 04:07:48 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const App = props => (
+	<p>
+		hello, world!
+	</p>
+)
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import App from "./App"
+import React from "react"
+import ReactDOM from "react-dom"
+
+import "minireset.css"
+import "material-colors.css"
+import "debug.css"
+
+ReactDOM.render(<App />, document.getElementById("root"))
