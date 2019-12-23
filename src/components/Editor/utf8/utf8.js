f1c1c70d5941f03872f4b0287f92b67c32613adc
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2019-12-23 19:01:35 +0900

    utf8 package

--- src/components/Editor/utf8/utf8.js
+++ src/components/Editor/utf8/utf8.js
@@ -1,31 +1,32 @@
-// 0x0009        CHARACTER TABULATION   h s
-// 0x000a              LINE FEED (LF)    vs
-// 0x000b             LINE TABULATION    vs
-// 0x000c              FORM FEED (FF)    vs
-// 0x000d        CARRIAGE RETURN (CR)    vs
-// 0x0020                       SPACE   h s
-// 0x0085             NEXT LINE (NEL)    vs
-// 0x00a0              NO-BREAK SPACE   h s
-// 0x1680            OGHAM SPACE MARK   h s
-// 0x180e   MONGOLIAN VOWEL SEPARATOR   h s
-// 0x2000                     EN QUAD   h s
-// 0x2001                     EM QUAD   h s
-// 0x2002                    EN SPACE   h s
-// 0x2003                    EM SPACE   h s
-// 0x2004          THREE-PER-EM SPACE   h s
-// 0x2005           FOUR-PER-EM SPACE   h s
-// 0x2006            SIX-PER-EM SPACE   h s
-// 0x2007                FIGURE SPACE   h s
-// 0x2008           PUNCTUATION SPACE   h s
-// 0x2009                  THIN SPACE   h s
-// 0x200a                  HAIR SPACE   h s
-// 0x2028              LINE SEPARATOR    vs
-// 0x2029         PARAGRAPH SEPARATOR    vs
-// 0x202f       NARROW NO-BREAK SPACE   h s
-// 0x205f   MEDIUM MATHEMATICAL SPACE   h s
-// 0x3000           IDEOGRAPHIC SPACE   h s
+// NOTE: This package assumes a maximum UTF-8 length of 4
+// bytes per Unicode character.
+//
+// // https://golang.org/pkg/unicode/utf8/#pkg-constants
+// const (
+//   RuneError = '\uFFFD'     // the "error" Rune or "Unicode replacement character"
+//   RuneSelf  = 0x80         // characters below Runeself are represented as themselves in a single byte.
+//   MaxRune   = '\U0010FFFF' // Maximum valid Unicode code point.
+//   UTFMax    = 4            // maximum number of bytes of a UTF-8 encoded Unicode character.
+// )
+//
+// This package is Unicode and emoji-friendly but does not
+// cover skin tones and compound graphemes.
 
-// `count` counts the UTF-8 length of a string.
+// `count` counts the UTF-8 length of a Unicode string.
 export function count(str) {
 	return [...str].length
+}
+
+// `countPrev` counts the UTF-8 length of the previous
+// character in a Unicode string.
+export function countPrev(str, index) {
+	const chars = [...str.slice(index - 4, index)]
+	return chars[chars.length - 1].length
+}
+
+// `countNext` counts the UTF-8 length of the next character
+// in a Unicode string.
+export function countNext(str, index) {
+	const chars = [...str.slice(index, index + 4)]
+	return chars[0].length
 }
---
+++ src/components/Editor/utf8/whitespace.js
@@ -0,0 +1,26 @@
+// 0x0009        CHARACTER TABULATION   h s
+// 0x000a              LINE FEED (LF)    vs
+// 0x000b             LINE TABULATION    vs
+// 0x000c              FORM FEED (FF)    vs
+// 0x000d        CARRIAGE RETURN (CR)    vs
+// 0x0020                       SPACE   h s
+// 0x0085             NEXT LINE (NEL)    vs
+// 0x00a0              NO-BREAK SPACE   h s
+// 0x1680            OGHAM SPACE MARK   h s
+// 0x180e   MONGOLIAN VOWEL SEPARATOR   h s
+// 0x2000                     EN QUAD   h s
+// 0x2001                     EM QUAD   h s
+// 0x2002                    EN SPACE   h s
+// 0x2003                    EM SPACE   h s
+// 0x2004          THREE-PER-EM SPACE   h s
+// 0x2005           FOUR-PER-EM SPACE   h s
+// 0x2006            SIX-PER-EM SPACE   h s
+// 0x2007                FIGURE SPACE   h s
+// 0x2008           PUNCTUATION SPACE   h s
+// 0x2009                  THIN SPACE   h s
+// 0x200a                  HAIR SPACE   h s
+// 0x2028              LINE SEPARATOR    vs
+// 0x2029         PARAGRAPH SEPARATOR    vs
+// 0x202f       NARROW NO-BREAK SPACE   h s
+// 0x205f   MEDIUM MATHEMATICAL SPACE   h s
+// 0x3000           IDEOGRAPHIC SPACE   h s
