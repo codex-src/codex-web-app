72e50b252b50c5893a205fac631a4ebf8fd35ba9
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2019-12-24 20:44:34 +0900

    small tweaks

--- src/components/Editor/DebugEditor.js
+++ src/components/Editor/DebugEditor.js
@@ -2,15 +2,17 @@ import React from "react"
 import stringify from "./stringify"
 import stylex from "stylex"
 
-const DebugEditor = stylex.Styleable(({ state, ...props }) => (
-	<pre style={stylex.parse("overflow -x:scroll")} {...props}>
-		<p style={{ ...stylex.parse("fs:12 lh:125%"), MozTabSize: 2, tabSize: 2, fontFamily: "'Monaco'" }}>
-			{stringify({
-				...state,
-				data: state.data.split(" ").join("·"),
-			})}
+const tabs = {
+	MozTabSize: 2, // Firefox.
+	tabSize: 2,    // etc.
+}
+
+const DebugEditor = props => (
+	<pre style={stylex.parse("overflow -x:scroll")}>
+		<p style={{ ...stylex.parse("fs:12 lh:125%"), ...tabs, fontFamily: "'Monaco'" }}>
+			{stringify(props.state)}
 		</p>
 	</pre>
-))
+)
 
 export default DebugEditor
