49097259fab4bef298a578867220a180824ce6eb
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2019-12-25 10:50:58 +0900

    vdom implementation draft 2

--- src/components/Editor/vdom/vdom.js
+++ src/components/Editor/vdom/vdom.js
@@ -1,85 +1,93 @@
-// invariant(
-// 	pos1 >= 0 && pos2 >= pos1 && pos2 <= this.count,
-// 	`VDOM: \`pos1=${pos1}\` and or \`pos2=${pos2}\` are out of bounds (\`count=${this.count}\`).`,
-// )
-
-// TODO: VDOM needs to parse each node with a key (and
-// component or component type in the future?) and for write
-// to update the current VDOM instead of blowing it away.
-// Also, VDOM needs to handle deleted, modififed, and added
-// nodes.
 class VDOM {
-	// state = {
-	// 	data:  "", // The VDOM’s data.
-	// 	nodes: [], // The VDOM’s data nodes.
-	// 	count: 0,  // The VDOM’s data count.
-	// }
 	constructor(data) {
+		const nodes = data.split("\n")
 		Object.assign(this, {
-			data,
-			nodes: data.split("\n"), // FIXME: Code blocks.
-			count: data.length,
+			data,  // The plain text data.
+			nodes, // The parsed nodes.
 		})
 	}
-	// `affected` computes the affected node and offset ranges
-	// for a selection of the VDOM.
-	affected(pos1, pos2) {
-		if (!pos1 && pos2 === this.count) {
-			return { node: [0, this.nodes.length], offset: [0, this.data.length] }
-		}
-		// FIXME
-		if (!(pos1 >= 0 && pos2 >= pos1 && pos2 <= this.count)) {
-			throw new Error(`VDOM: \`pos1=${pos1}\` and or \`pos2=${pos2}\` are out of bounds (\`count=${this.count}\`).`)
-		}
-		let index = pos1
+	// `read` reads the VDOM.
+	read(pos1 = 0, pos2 = this.data.length) { // Arguments can be omitted.
+		return this.data.slice(pos1, pos2)
+	}
+	// `write` writes to the VDOM.
+	write(data, pos1, pos2) {
+
+		// Start node and offset:
+		let pos = pos1
 		let node1 = 0
 		let offset1 = 0
 		while (node1 < this.nodes.length) {
-			if (index - this.nodes[node1].length <= 0) {
-				offset1 = index // Offset from the start (zero-based).
+			if (pos - this.nodes[node1].length <= 0) {
+				// Offset from the start:
+				offset1 = pos
 				break
 			}
-			index -= this.nodes[node1].length
+			pos -= this.nodes[node1].length
 			if (node1 + 1 < this.nodes[node1].length) {
-				index--
+				pos--
 			}
 			node1++
 		}
-		index = pos2 - pos1
+
+		// End node and offset (inverted):
+		pos = pos2 - pos1
 		let node2 = node1
 		let offset2 = 0
 		while (node2 < this.nodes.length) {
-			if (index - this.nodes[node2].length <= 0) {
-				offset2 = index - this.nodes[node2].length // Offset from the end (zero-based).
+			if (pos - this.nodes[node2].length <= 0) {
+				// Offset from the end:
+				offset2 = pos - this.nodes[node2].length
 				break
 			}
-			index -= this.nodes[node2].length
+			pos -= this.nodes[node2].length
 			if (node2 + 1 < this.nodes[node2].length) {
-				index--
+				pos--
 			}
 			node2++
 		}
+
 		const range = {
 			node: [node1, node2 + 1],
 			offset: [offset1, offset2 + 1],
 		}
-		return range
-	}
-	// `read` reads a selection of the VDOM.
-	read(pos1 = 0, pos2 = this.count) { // Arguments can be omitted.
-		if (!pos1 && pos2 === this.count) {
-			return this.data
-		}
-		const { node, offset } = this.affected(pos1, pos2)
-		const chunk = this.nodes.slice(...node).join("\n")
-		if (!offset[1]) {
-			offset[1] = chunk.length
-		}
-		return chunk.slice(...offset)
-	}
-	// `write` writes data at a selection to the VDOM.
-	write(data, pos1, pos2) {
+
 		const newData = this.data.slice(0, pos1) + data + this.data.slice(pos2)
-		Object.assign(this, new VDOM(newData))
+		const newNodes = []
+
+		// Start node; must have one or more nodes:
+		const nodes = data.split("\n")
+		if (nodes.length) {
+			newNodes.push(this.nodes[range.node[0]].slice(0, range.offset[0]) + nodes[0])
+		}
+		// Intermediary nodes; must have three or more nodes:
+		if (nodes.length > 2) {
+			newNodes.push(...nodes.slice(1, -1))
+		}
+		// End node; must have two or more nodes:
+		if (nodes.length > 1) {
+			newNodes.push(nodes.slice(-1)[0] + this.nodes[range.node[1]].slice(range.offset[1]))
+		}
+
+		Object.assign(this, {
+			data: newData,
+			nodes: newNodes,
+		})
 	}
+
 }
+
+const v = new VDOM("foo\nbar\nbaz")
+v.write("abc\ndef\nghi", 2, 9)
+
+// foo
+// bar
+// baz
+//
+// abc
+// def
+// ghi
+//
+// foabc
+// def
+// ghiaz
