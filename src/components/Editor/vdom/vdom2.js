54ac85aa89dcddd0944f0bf392a311c517623eee
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2019-12-26 12:20:28 +0900

    add full test suite (15 tests) for vdom - passing

--- src/components/Editor/vdom/vdom2.js
+++ src/components/Editor/vdom/vdom2.js
@@ -1,14 +1,5 @@
-// https://gist.github.com/jed/982883
-//
-// uuidv4() // 96ace54c-69b0-4a9c-a8d5-7a22b111d05b
-// uuidv4() // 4e27fdb6-d5ba-4825-8f2b-a975eb96ed6f
-// uuidv4() // 5f542e0f-f0d0-402c-b246-d7e049ec648e
-//
-function uuidv4(a) {
-	return a
-		? (a ^ Math.random() * 16 >> a / 4).toString(16)
-		: ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, uuidv4)
-}
+import invariant from "invariant"
+import uuidv4 from "uuid/v4"
 
 // `parseVDOMNodes` parses plain text data into VDOM nodes.
 function parseVDOMNodes(data) {
@@ -18,11 +9,6 @@ function parseVDOMNodes(data) {
 	}))
 	return nodes
 }
-
-// invariant(
-// 	pos1 >= 0 && pos2 >= pos1 && pos2 <= this.data.length,
-// 	`vdom: \`pos1\` and or \`pos2\` are out of bounds.`,
-// )
 
 // A `VDOM` represents a VDOM for plain text data;
 // paragraphs are parsed into VDOM nodes. A VDOM node is a
@@ -95,12 +81,24 @@ class VDOM {
 	}
 	// `write` writes plain text data at a selection.
 	write(data, pos1, pos2) {
-		if (!pos1 && pos2 === data.length) {
+		invariant(
+			pos1 >= 0 && pos2 >= pos1 && pos2 <= this.data.length,
+			`vdom: \`write\` out of bounds: \`${0} <= ${pos1} <= ${pos2} <= ${this.data.length}\`.`,
+		)
+
+		if (!pos1 && pos2 === this.data.length) {
 			return new VDOM(data)
 		}
-		const newNodes = []                                    // The new nodes.
-		const { start, end } = this._affectedRange(pos1, pos2) // The affected range.
-		const parsedNodes = parseVDOMNodes(data)               // The parsed nodes from the plain text data.
+
+		// The new data.
+		const newData = this.data.slice(0, pos1) + data + this.data.slice(pos2)
+		// The affected range.
+		const { start, end } = this._affectedRange(pos1, pos2)
+		// The parsed nodes from the plain text data.
+		const parsedNodes = parseVDOMNodes(data)
+		// The new nodes.
+		const newNodes = []
+
 		// Push the pre-start nodes:
 		if (start.node > 0) {
 			newNodes.push(...[...this.nodes.slice(0, start.node)])
@@ -123,90 +121,8 @@ class VDOM {
 		if (end.node + 1 < this.nodes.length) {
 			newNodes.push(...[...this.nodes.slice(end.node + 1)])
 		}
-		// Return merged VDOM (shared keys):
-		return new VDOM(data, newNodes)
+		return new VDOM(newData, newNodes)
 	}
 }
 
-// Tests to write:
-//
-// write at ends
-//
-// 	a -> ba
-//
-// 	a -> ab
-//
-// overwite
-//
-// 	abc -> dbc
-//
-// 	abc -> adc
-//
-// 	abc -> abd
-//
-// write at ends (multiline)
-//
-// 	a -> ca
-// 	b    b
-//
-// 	a -> a
-// 	b    bc
-//
-// overwrite (multiline)
-//
-// 	aaa -> daa
-// 	bbb    bbb
-// 	ccc    ccc
-//
-// 	aaa -> aaa
-// 	bbb    bdb
-// 	ccc    ccc
-//
-// 	aaa -> aaa
-// 	bbb    bbb
-// 	ccc    ccd
-//
-// write at ends (multiline input)
-//
-// 	a -> c
-// 	b    da
-// 	     b
-//
-// 	a -> a
-// 	b    bc
-// 	     d
-//
-// overwrite (multiline input)
-//
-// 	aaa -> ddd
-// 	bbb    eee
-// 	ccc    fffaa
-// 	       bbb
-// 	       ccc
-//
-// 	aaa -> aaa
-// 	bbb    bddd
-// 	ccc    eee
-// 	       fffb
-// 	       ccc
-//
-// 	aaa -> aaa
-// 	bbb    bbb
-// 	ccc    ccddd
-// 	       eee
-// 	       fff
-
-// 0  foo     foo
-// 1  bar[    barqoozqux
-// 2  baz  -> quux
-// 3 ]qux
-// 4  quux
-
-let v = new VDOM(`aaa
-bbb
-ccc
-ddd
-eee`)
-v.write(`xxx
-yyy
-zzz`, 7, 8)
+export default VDOM
