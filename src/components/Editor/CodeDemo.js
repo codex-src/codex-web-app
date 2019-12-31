a485d1e30b36d7f9065d584d0131373c28139a90
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-01-01 03:49:13 +0900

    improvements to code demo

--- src/components/Editor/CodeDemo.js
+++ src/components/Editor/CodeDemo.js
@@ -1,5 +1,6 @@
+// import stylex from "stylex"
 import React from "react"
-import stylex from "stylex"
+import ReactDOM from "react-dom"
 
 import "./code-demo.css"
 
@@ -11,86 +12,109 @@ function isBreakNode(node) {
 	return ok
 }
 
-// read_root reads the text from from a root
-function read_root(root) {
+// `isBreakOrTextNode` returns whether a node is a break
+// node or a text node.
+function isBreakOrTextNode(node) {
+	const ok = (
+		(node.nodeType === Node.ELEMENT_NODE && node.nodeName === "BR") ||
+		node.nodeType === Node.TEXT_NODE
+	)
+	return ok
+}
+
+// `nodeValue` mocks the browser function; reads from a
+// break or text node.
+export function nodeValue(node) {
+	if (!isBreakOrTextNode(node)) {
+		return ""
+	}
+	// Convert non-breaking spaces:
+	return (node.nodeValue || "").replace("\u00a0", " ")
+}
+
+// `innerText` mocks the browser function; recursively reads
+// from a root node.
+function innerText(rootNode) {
 	let value = ""
-	const recurse = function(node) {
-		for (const child of node.childNodes) {
-			const nested = child.parentNode !== root
-			if (nested && (isBreakNode(child) || child.nodeType === Node.TEXT_NODE)) {
-				value += isBreakNode(child) ? "" : child.nodeValue
+	const recurse = startNode => {
+		for (const currentNode of startNode.childNodes) {
+			const nested = currentNode.parentNode !== rootNode
+			if (nested && isBreakOrTextNode(currentNode)) {
+				value += nodeValue(currentNode)
 			} else {
-				recurse(child)
-				if (!nested && child.nodeType === Node.ELEMENT_NODE &&
-						child.nextElementSibling) {
+				recurse(currentNode)
+				if (!nested && currentNode.nodeType === Node.ELEMENT_NODE &&
+						currentNode.nextElementSibling) {
 					value += "\n"
 				}
 			}
 		}
 	}
-	recurse(root)
+	recurse(rootNode)
 	return value
 }
 
-// find_pos finds the absolute position from a node
-function find_pos(root, node, offset) {
+// `computeVDOMPos` computes the VDOM cursor.
+function computeVDOMPos(rootNode, node, offset) {
 	let pos = 0
 	if (node && !isBreakNode(node) && node.nodeType === Node.ELEMENT_NODE) {
-		return find_pos(root, node.childNodes[offset], 0)
+		return computeVDOMPos(rootNode, node.childNodes[offset], 0)
 	}
-	const recurse = function(_node) {
-		for (const child of _node.childNodes) {
-			const nested = child.parentNode !== root
-			if (nested && (isBreakNode(child) || child.nodeType === Node.TEXT_NODE)) {
-				if (child === node) {
+	const recurse = startNode => {
+		for (const currentNode of startNode.childNodes) {
+			const nested = currentNode.parentNode !== rootNode
+			if (nested && isBreakOrTextNode(currentNode)) {
+				if (currentNode === node) {
 					return true
 				}
-				pos += isBreakNode(child) ? 0 : child.nodeValue.length
+				pos += nodeValue(currentNode).length
 			} else {
-				if (recurse(child)) {
+				if (recurse(currentNode)) {
 					return true
-				}
-				if (!nested && child.nodeType === Node.ELEMENT_NODE &&
-						child.nextElementSibling) {
+				} else if (!nested && currentNode.nodeType === Node.ELEMENT_NODE &&
+						currentNode.nextElementSibling) {
 					pos += 1
 				}
 			}
 		}
 		return false
 	}
-	recurse(root)
+	recurse(rootNode)
 	return pos + offset
 }
 
-// find_node finds the node from an absolute position
-function find_node(root, pos) {
-	let node = root
-	let offset = 0
-	const recurse = function(_node) {
-		for (const child of _node.childNodes) {
-			const nested = child.parentNode !== root
-			if (nested && (isBreakNode(child) || child.nodeType === Node.TEXT_NODE)) {
-				const length = isBreakNode(child) ? 0 : child.nodeValue.length
+// `computeDOMPos` computes the DOM cursor.
+function computeDOMPos(rootNode, pos) {
+	const node = {
+		node: rootNode,
+		offset: 0,
+	}
+	const recurse = startNode => {
+		for (const currentNode of startNode.childNodes) {
+			const nested = currentNode.parentNode !== rootNode
+			if (nested && isBreakOrTextNode(currentNode)) {
+				const { length } = nodeValue(currentNode)
 				if (pos - length <= 0) {
-					node = child
-					offset = pos
+					Object.assign(node, {
+						node: currentNode,
+						offset: pos,
+					})
 					return true
 				}
 				pos -= length
 			} else {
-				if (recurse(child)) {
+				if (recurse(currentNode)) {
 					return true
-				}
-				if (!nested && child.nodeType === Node.ELEMENT_NODE &&
-						child.nextElementSibling) {
+				} else if (!nested && currentNode.nodeType === Node.ELEMENT_NODE &&
+						currentNode.nextElementSibling) {
 					pos -= 1
 				}
 			}
 		}
 		return false
 	}
-	recurse(root)
-	return { node, offset }
+	recurse(rootNode)
+	return node
 }
 
 /*
@@ -107,11 +131,10 @@ class Editor {
 			pos2:  0, // The cursor end.
 		})
 
-		this.setValue()
+		this.setValue() // E.g. render?
 		this.mount()
 	}
 	mount() {
-		// Tab:
 		this.el.addEventListener("keydown", e => {
 			if (e.key !== "Tab") {
 				// No-op.
@@ -120,9 +143,7 @@ class Editor {
 			e.preventDefault()
 			document.execCommand("insertText", false, "\t")
 		})
-		// Shift-enter:
 		this.el.addEventListener("keydown", e => {
-			const guard = !e.shiftKey || e.key !== "Enter"
 			if (!e.shiftKey || e.key !== "Enter") {
 				// No-op.
 				return
@@ -130,7 +151,6 @@ class Editor {
 			e.preventDefault()
 			document.execCommand("insertText", false, "\n")
 		})
-		// Input:
 		this.el.addEventListener("input", e => {
 			if (e.inputType === "insertCompositionText") {
 				// No-op.
@@ -140,7 +160,7 @@ class Editor {
 			this.update()
 			const { value, pos1, pos2 } = this
 			if (!value.length) {
-				this.setValue("\n")
+				this.setValue("\n") // The insertion point node.
 				return
 			}
 			if (pos1 === pos2 && pos2 === value.length) {
@@ -149,12 +169,11 @@ class Editor {
 			this.render()
 		})
 	}
-
 	/*
 	 * value
 	 */
 	getValue() {
-		this.value = read_root(this.el)
+		this.value = innerText(this.el)
 	}
 	setValue(value) {
 		if (value !== undefined) {
@@ -172,8 +191,8 @@ class Editor {
 	 */
 	getPos() {
 		const selection = document.getSelection()
-		this.pos1 = find_pos(this.el, selection.anchorNode, selection.anchorOffset)
-		this.pos2 = find_pos(this.el, selection.focusNode, selection.focusOffset)
+		this.pos1 = computeVDOMPos(this.el, selection.anchorNode, selection.anchorOffset)
+		this.pos2 = computeVDOMPos(this.el, selection.focusNode, selection.focusOffset)
 	}
 	setPos(pos1, pos2) {
 		if (pos1 !== undefined && pos2 !== undefined) {
@@ -181,8 +200,8 @@ class Editor {
 			this.pos2 = pos2
 		}
 		const selection = document.getSelection()
-		const node1 = find_node(this.el, this.pos1)
-		const node2 = find_node(this.el, this.pos2)
+		const node1 = computeDOMPos(this.el, this.pos1)
+		const node2 = computeDOMPos(this.el, this.pos2)
 		const range = document.createRange()
 		range.setStart(node1.node, node1.offset)
 		range.setEnd(node2.node, node2.offset)
@@ -214,16 +233,18 @@ const Token = {
 
 class Lexer {
 	constructor(value) {
-		this.value = value
-		this.x1 = 0
-		this.x2 = 0
-		this.width = 0
-		this.lines = [[]]
+		Object.assign(this, {
+			value,
+			x1:    0,
+			x2:    0,
+			width: 0,
+			lines: [[]],
+		})
 	}
 	next() {
 		if (this.x2 === this.value.length) {
 			this.width = 0
-			return undefined
+			return undefined // E.g. EOF.
 		}
 		const ch = this.value[this.x2]
 		this.width = 1
@@ -240,7 +261,10 @@ class Lexer {
 	}
 	emit(token) {
 		const nth = this.lines.length - 1
-		this.lines[nth].push({ token, value: this.focus() })
+		this.lines[nth].push({
+			token,
+			value: this.focus(),
+		})
 		this.ignore()
 	}
 	emit_line(token) {
@@ -257,7 +281,21 @@ class Lexer {
 		this.x1 = this.x2
 	}
 	accept(str) {
-		return str.includes(this.next()) || !!this.backup()
+		// const ch = this.next()
+		// if (str.includes(ch)) {
+		// 	return true
+		// }
+		// this.backup()
+		// return false
+
+		const nextCh = this.next()
+		const ok = str.includes(nextCh)
+		if (!ok) {
+			this.backup()
+		}
+		return ok
+
+		// return str.includes(this.next()) || !!this.backup()
 	}
 	accept_run(str) {
 		while (this.accept(str)) {
@@ -266,71 +304,71 @@ class Lexer {
 	}
 }
 
-const key_map = {
-	"break":       true,
-	"default":     true,
-	"func":        true,
-	"interface":   true,
-	"select":      true,
-	"case":        true,
-	"defer":       true,
-	"go":          true,
-	"map":         true,
-	"struct":      true,
-	"chan":        true,
-	"else":        true,
-	"goto":        true,
-	"package":     true,
-	"switch":      true,
-	"const":       true,
-	"fallthrough": true,
-	"if":          true,
-	"range":       true,
-	"type":        true,
-	"continue":    true,
-	"for":         true,
-	"import":      true,
-	"return":      true,
-	"var":         true,
-	"bool":        true,
-	"byte":        true,
-	"complex64":   true,
-	"complex128":  true,
-	"error":       true,
-	"float32":     true,
-	"float64":     true,
-	"int":         true,
-	"int8":        true,
-	"int16":       true,
-	"int32":       true,
-	"int64":       true,
-	"rune":        true,
-	"string":      true,
-	"uint":        true,
-	"uint8":       true,
-	"uint16":      true,
-	"uint32":      true,
-	"uint64":      true,
-	"uintptr":     true,
-	"true":        true,
-	"false":       true,
-	"iota":        true,
-	"nil":         true,
-	"append":      true,
-	"cap":         true,
-	"close":       true,
-	"complex":     true,
-	"copy":        true,
-	"delete":      true,
-	"imag":        true,
-	"len":         true,
-	"make":        true,
-	"new":         true,
-	"panic":       true,
-	"print":       true,
-	"println":     true,
-	"real":        true,
-	"recover":     true,
+const keywords = {
+	break:       true,
+	default:     true,
+	func:        true,
+	interface:   true,
+	select:      true,
+	case:        true,
+	defer:       true,
+	go:          true,
+	map:         true,
+	struct:      true,
+	chan:        true,
+	else:        true,
+	goto:        true,
+	package:     true,
+	switch:      true,
+	const:       true,
+	fallthrough: true,
+	if:          true,
+	range:       true,
+	type:        true,
+	continue:    true,
+	for:         true,
+	import:      true,
+	return:      true,
+	var:         true,
+	bool:        true,
+	byte:        true,
+	complex64:   true,
+	complex128:  true,
+	error:       true,
+	float32:     true,
+	float64:     true,
+	int:         true,
+	int8:        true,
+	int16:       true,
+	int32:       true,
+	int64:       true,
+	rune:        true,
+	string:      true,
+	uint:        true,
+	uint8:       true,
+	uint16:      true,
+	uint32:      true,
+	uint64:      true,
+	uintptr:     true,
+	true:        true,
+	false:       true,
+	iota:        true,
+	nil:         true,
+	append:      true,
+	cap:         true,
+	close:       true,
+	complex:     true,
+	copy:        true,
+	delete:      true,
+	imag:        true,
+	len:         true,
+	make:        true,
+	new:         true,
+	panic:       true,
+	print:       true,
+	println:     true,
+	real:        true,
+	recover:     true,
 }
 
 function lex(value) {
@@ -339,7 +377,7 @@ function lex(value) {
 	while ((ch = lexer.next())) {
 		let token = 0
 		switch (true) {
-		// comment
+		// Comment:
 		case ch === "/" && (lexer.peek() === "/" || lexer.peek() === "*"):
 			ch = lexer.next()
 			if (ch === "/") {
@@ -356,13 +394,13 @@ function lex(value) {
 						break
 					} else if (ch === "\n") {
 						lexer.emit_line(Token.COM)
-						// don't break
+						// Don't break.
 					}
 				}
 			}
 			token = Token.COM
 			break
-			// whitespace
+			// Whitespace:
 		case ch === " " || ch === "\t" || ch === "\n":
 			if (lexer.x2 > 1 && ch === "\n") {
 				lexer.lines.push([])
@@ -370,11 +408,12 @@ function lex(value) {
 				break
 			}
 			lexer.accept_run(" \t")
+			// token = Token.UNS
 			break
-			// keyword or function
+			// Keyword or function:
 		case (ch >= "a" && ch <= "z") || (ch >= "A" && ch <= "Z") || ch === "_":
 			lexer.accept_run("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789")
-			if (key_map[lexer.focus()]) {
+			if (keywords[lexer.focus()]) {
 				token = Token.KEY
 				break
 			}
@@ -386,7 +425,7 @@ function lex(value) {
 			lexer.x2 = x2
 			token = token || Token.UNS
 			break
-			// string
+			// String:
 		case ch === "'" || ch === "\"" || ch === "`":
 			const quote = ch
 			while ((ch = lexer.next())) {
@@ -404,7 +443,7 @@ function lex(value) {
 			}
 			token = Token.STR
 			break
- 			// number
+ 			// Number:
 		case ch >= "0" && ch <= "9":
 			let base = "0123456789"
 			if (lexer.accept("0") && lexer.accept("xX")) {
@@ -416,12 +455,12 @@ function lex(value) {
 			lexer.accept("i")
 			token = Token.NUM
 			break
-			// punctuation
+			// Punctuation:
 		case "!%&()*+,-./:;<=>[]^{|}".includes(ch):
 			lexer.accept_run("!%&()*+,-./:;<=>[]^{|}")
 			token = Token.PUN
 			break
-			// not whitespace
+			// Non-whitespace:
 		default:
 			while ((ch = lexer.next())) {
 				if (ch === " " || ch === "\t" || ch === "\n") {
@@ -440,42 +479,51 @@ function lex(value) {
 }
 
 function parse(lines) {
+	const CodeBlock = props => ((
+		props.children.map((line, index) => (
+			<code key={index} data-vdom-node>
+				{!line.length && (
+					<br />
+				)}
+
+				{line.map((item, index) => (
+					!item.token ? (
+						item.value
+					) : (
+						<span key={index} className={item.token}>
+							{item.value || (
+								<br />
+							)}
+						</span>
+					)
+				))}
+			</code>
+		))
+	))
 	const fragment = document.createDocumentFragment()
-	for (const line of lines) {
-		const el = document.createElement("code")
-		if (!line.length) {
-			el.appendChild(document.createElement("br"))
-		}
-		for (const item of line) {
-			let node = document.createTextNode(item.value) // assumes no token
-			if (item.token) {
-				node = document.createElement("span")
-				node.classList.add(item.token)
-				node.appendChild(!item.value ? document.createElement("br")
-					: document.createTextNode(item.value))
-			}
-			el.appendChild(node)
-		}
-		fragment.appendChild(el)
-	}
+	ReactDOM.render(<CodeBlock>{lines}</CodeBlock>, fragment)
 	return fragment
 }
 
-/*
- * main
- */
 const EditorComponent = props => (
-	<div style={stylex.parse("flex -r -x:center")}>
-		<div className="editor" style={stylex.parse("w:640")} contentEditable suppressContentEditableWarning spellCheck={false}>
-			<code>
-				<span className="com">{"// hello, world!"}</span>
-			</code>
-		</div>
-	</div>
+	<article
+		style={{ MozTabSize: 2, tabSize: 2, font: "15px/1.375 Monaco" }}
+		contentEditable
+		suppressContentEditableWarning
+		spellCheck={false} // FIXME: Remove.
+		data-vdom-root
+	>
+		<code>
+			{/* Placeholder: */}
+			<span className="com">
+				{"// hello, world!"}
+			</span>
+		</code>
+	</article>
 )
 
 document.addEventListener("DOMContentLoaded", () => {
-	const editor = new Editor(".editor", `package main\n\nimport "fmt"\n\nfunc main() {\n	fmt.Println("hello, world!")\n}`)
+	new Editor("[data-vdom-root]", "package main\n\nimport \"fmt\"\n\nfunc main() {\n	fmt.Println(\"hello, world!\")\n}")
 })
 
 export default EditorComponent
