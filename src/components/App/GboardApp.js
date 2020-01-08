8b24f55ede675c57699ce3f8264ead4d35999538
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-01-09 08:36:39 +0900

    The superfluous Android selection bug has been identifier
    The selection bug as noted in the previous commit described a naked `contentEditable` bug.
    
    When pressing delete on:
    
    hello
    <cursor>hello
    
    Android spits out:
    
    helloh<cursor>ello
    
    It turns out that this somehow relates to using semantic HTML elements, as supposed to `<div>`, which is as boring as it gets.
    
    The fix for this bug was identified in the client source code of notion.so; Notion appears to use `div`s everywhere imaginable. Therefore, semantic elements are **not** recommended where possible.

--- src/components/App/GboardApp.js
+++ src/components/App/GboardApp.js
@@ -7,10 +7,30 @@ const GboardApp = props => (
 	<DebugCSS>
 		<div style={stylex.parse("p-x:24 p-y:32 flex -r -x:center")}>
 			<div style={stylex.parse("w:1024")}>
-				<Editor.Editor />
+				<article contentEditable suppressContentEditableWarning>
+					<p>
+						hellos
+					</p>
+					<p>
+						hello
+					</p>
+					<p>
+						hello
+					</p>
+				</article>
 			</div>
 		</div>
 	</DebugCSS>
 )
+
+// const GboardApp = props => (
+// 	<DebugCSS>
+// 		<div style={stylex.parse("p-x:24 p-y:32 flex -r -x:center")}>
+// 			<div style={stylex.parse("w:1024")}>
+// 				<Editor.Editor />
+// 			</div>
+// 		</div>
+// 	</DebugCSS>
+// )
 
 export default GboardApp
