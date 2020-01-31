7659895cd57e041e20ac44f5a5c74e17b37b2828
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-01-31 20:24:45 +0900

    Added support for the reset pattern and rendering React components to a hidden DOM

--- src/components/FirefoxEditor/Editor.js
+++ src/components/FirefoxEditor/Editor.js
@@ -2,6 +2,7 @@ import CSSDebugger from "utils/CSSDebugger"
 import Enum from "utils/Enum"
 import platform from "utils/platform"
 import React from "react"
+import ReactDOM from "react-dom"
 import stylex from "stylex"
 import useMethods from "use-methods"
 
@@ -27,14 +28,15 @@ const initialState = {
 	pos1: 0,
 	pos2: 0,
 	collapsed: false,
+	reset: null,
 	components: null,
+	shouldRender: 0,
+	reactDOM: null,
 }
-
-// const EPOCH = Date.now()
 
 const reducer = state => ({
 	newAction(actionType) {
-		const actionTimeStamp = Date.now() // - EPOCH
+		const actionTimeStamp = Date.now()
 		if (actionType === ActionTypes.SELECT && actionTimeStamp - state.actionTimeStamp < 100) {
 			// No-op
 			return
@@ -54,13 +56,13 @@ const reducer = state => ({
 		const collapsed = pos1 === pos2
 		Object.assign(state, { pos1, pos2, collapsed })
 	},
-	actionInput(data, pos1, pos2) {
+	actionInput(data, pos1, pos2, reset) {
 		this.newAction(ActionTypes.INPUT)
-		Object.assign(state, { data, pos1, pos2 })
-		// TODO
+		Object.assign(state, { data, pos1, pos2, reset })
+		this.render()
 	},
-	actionInputDropBytes(dropL, dropR) { // dropL and dropR are expected to be >= 0
-		this.newAction(ActionTypes.INPUT)
+	// NOTE: dropL and dropR are expected to be >= 0
+	actionInputDropBytes(dropL, dropR) {
 		if (!state.collapsed) {
 			dropL = 0
 			dropR = 0
@@ -68,8 +70,7 @@ const reducer = state => ({
 		const data = state.data.slice(0, state.pos1 - dropL) + state.data.slice(state.pos2 + dropR)
 		const pos1 = state.pos1 - dropL
 		const pos2 = pos1
-		Object.assign(state, { data, pos1, pos2 })
-		// TODO
+		this.actionInput(data, pos1, pos2)
 	},
 	backspaceL() {
 		this.actionInputDropBytes(1, 0)
@@ -77,18 +78,26 @@ const reducer = state => ({
 	backspaceR() {
 		this.actionInputDropBytes(0, 1)
 	},
+	render() {
+		state.components = parseComponents(state.data)
+		state.shouldRender++
+	},
 })
 
-const init = initialValue => initialState => {
-	const state = {
-		...initialState,
-		actionType: ActionTypes.INIT,
-		actionTimeStamp: Date.now(),
-		data: initialValue,
-		components: parseComponents(initialValue),
-	}
-	return state
-}
+const init = initialValue => initialState => ({
+	...initialState,
+	actionType: ActionTypes.INIT,
+	actionTimeStamp: Date.now(),
+	data: initialValue,
+	reset: {
+		id: "",
+		textOffset: 0,
+	},
+	components: parseComponents(initialValue),
+	reactDOM: document.createElement("div"),
+})
+
+const useEditor = initialValue => useMethods(reducer, initialState, init(initialValue))
 
 // Gets cursors from a range.
 function getPosFromRange(rootNode, node, offset) {
@@ -122,7 +131,7 @@ function getPosFromRange(rootNode, node, offset) {
 	return pos
 }
 
-// Gets plain text data; recursively reads from a root node.
+// Gets (recursively reads) data from a root node.
 //
 // TODO: Is arr.join("\n") faster?
 function getData(rootNode) {
@@ -149,17 +158,113 @@ function getData(rootNode) {
 	return data
 }
 
+// Gets the text offset from a (of a) range; code based on
+// getPosFromRange.
+function getTextOffsetFromRange(rootNode, node, offset) {
+	let textOffset = 0
+	const recurse = startNode => {
+		const { childNodes } = startNode
+		let index = 0
+		while (index < childNodes.length) {
+			if (childNodes[index] === node) {
+				textOffset += offset
+				return true
+			}
+			textOffset += (childNodes[index].nodeValue || "").length
+			if (recurse(childNodes[index])) {
+				return true
+			}
+			const { nextSibling } = childNodes[index]
+			if (nextSibling && nextSibling.hasAttribute("data-node")) {
+				textOffset++
+			}
+			index++
+		}
+		return false
+	}
+	recurse(rootNode)
+	return textOffset
+}
+
+// Gets the range from a text offset; code based on
+// getPosFromRange.
+function getRangeFromTextOffset(rootNode, textOffset) {
+	let node = null
+	let offset = 0
+	const recurse = startNode => {
+		const { childNodes } = startNode
+		let index = 0
+		while (index < childNodes.length) {
+			const { length } = childNodes[index].nodeValue || ""
+			if (textOffset - length <= 0) {
+				node = childNodes[index] // Set
+				offset = textOffset      // Reset
+				return true
+			}
+			textOffset -= length
+			if (recurse(childNodes[index])) {
+				return true
+			}
+			const { nextSibling } = childNodes[index]
+			if (nextSibling && nextSibling.hasAttribute("data-node")) {
+				textOffset--
+			}
+			index++
+		}
+		return false
+	}
+	recurse(rootNode)
+	return { node, offset }
+}
+
+// // Gets a range for a key node and offset.
+// function getRangeFromKeyNodeAndOffset(keyNode, offset) {
+// 	const range = {
+// 		node: null,
+// 		offset: 0,
+// 	}
+// 	const recurseOn = startNode => {
+// 		for (const currentNode of startNode.childNodes) {
+// 			if (isTextOrBreakNode(currentNode)) {
+// 				// If found, return:
+// 				const { length } = nodeValue(currentNode)
+// 				if (offset - length <= 0) {
+// 					Object.assign(range, {
+// 						node: currentNode,
+// 						offset,
+// 					})
+// 					return true
+// 				}
+// 				offset -= length
+// 			} else {
+// 				// If found recursing on the current node, return:
+// 				if (recurseOn(currentNode)) {
+// 					return true
+// 				}
+// 			}
+// 		}
+// 		return false
+// 	}
+// 	recurseOn(keyNode)
+// 	return range
+// }
+
 const Paragraph = props => (
-	<div data-node>
+	<div id={props.reactKey} data-node>
 		{props.children || (
 			<br />
 		)}
 	</div>
 )
 
+const key = index => ({
+	key: index,
+	reactKey: index,
+})
+
 function parseComponents(data) {
 	const components = data.split("\n").map((each, index) => (
-		<Paragraph key={index}>
+		<Paragraph { ...key(index) }>
 			{each}
 		</Paragraph>
 	))
@@ -184,27 +289,73 @@ function isBackspaceRMacOS(e) {
 	return ok
 }
 
+function FirefoxEditorComponents(props) {
+	return props.components
+}
+
 function FirefoxEditor(props) {
-	const rootNodeRef = React.useRef()
+	const ref = React.useRef()
 	const isPointerDownRef = React.useRef()
 	const FFDedupeCompositionEndRef = React.useRef()
 
-	const [state, dispatch] = useMethods(reducer, initialState, init(`Hello
+	const [state, dispatch] = useEditor(`Hello
 
 Hello
 
-Hello`))
+Hello`)
 
-	// Gets cursors.
+	React.useLayoutEffect(
+		React.useCallback(() => {
+			// TODO: Try ReactDOM.render(state.components, state.reactDOM, () => { ... })
+			ReactDOM.render(<FirefoxEditorComponents components={state.components} />, state.reactDOM, () => {
+				if (!state.shouldRender) {
+					// ;[...ref.current.childNodes].map(each => each.remove())
+					ref.current.append(...state.reactDOM.cloneNode(true).childNodes)
+					return
+				}
+				;[...ref.current.childNodes].map(each => each.remove())
+				ref.current.append(...state.reactDOM.cloneNode(true).childNodes)
+
+				const rootNode = document.getElementById(state.reset.id) // FIXME: Compound components
+				const { node, offset } = getRangeFromTextOffset(rootNode, state.reset.textOffset)
+
+				const selection = document.getSelection()
+				const range = document.createRange()
+				range.setStart(node, offset)
+				range.collapse() // FIXME: range.setEnd(...)
+				selection.removeAllRanges()
+				selection.addRange(range)
+			})
+		}, [state]),
+		[state.shouldRender],
+	)
+
+	// Gets the cursors.
 	const getPos = () => {
 		const selection = document.getSelection()
 		const range = selection.getRangeAt(0)
-		const pos1 = getPosFromRange(rootNodeRef.current, range.startContainer, range.startOffset)
+		const pos1 = getPosFromRange(ref.current, range.startContainer, range.startOffset)
 		let pos2 = pos1
 		if (!range.collapsed) {
-			pos2 = getPosFromRange(rootNodeRef.current, range.endContainer, range.endOffset)
+			pos2 = getPosFromRange(ref.current, range.endContainer, range.endOffset)
 		}
 		return [pos1, pos2]
+	}
+
+	// Gets the reset (ID and text offset).
+	const getReset = () => {
+		const selection = document.getSelection()
+		const range = selection.getRangeAt(0)
+		let node = range.startContainer
+		while (node) {
+			if (node.nodeType === Node.ELEMENT_NODE && node.hasAttribute("data-node")) {
+				break
+			}
+			node = node.parentNode
+		}
+		const { id } = node
+		const textOffset = getTextOffsetFromRange(node, range.startContainer, range.startOffset)
+		return { id, textOffset }
 	}
 
 	return (
@@ -212,7 +363,7 @@ Hello`))
 			{React.createElement(
 				"div",
 				{
-					ref: rootNodeRef,
+					ref,
 
 					contentEditable: true,
 					suppressContentEditableWarning: true,
@@ -230,14 +381,14 @@ Hello`))
 						// NOTE: Select all (command-a or control-a) in
 						// Gecko/Firefox selects the root node instead
 						// of the innermost start and end nodes
-						if (range.commonAncestorContainer === rootNodeRef.current) {
+						if (range.commonAncestorContainer === ref.current) {
 							// Iterate to the innermost start node:
-							let startNode = rootNodeRef.current.childNodes[0]
+							let startNode = ref.current.childNodes[0]
 							while (startNode.childNodes.length) {
 								startNode = startNode.childNodes[0]
 							}
 							// Iterate to the innermost end node:
-							let endNode = rootNodeRef.current.childNodes[rootNodeRef.current.childNodes.length - 1]
+							let endNode = ref.current.childNodes[ref.current.childNodes.length - 1]
 							while (endNode.childNodes.length) {
 								endNode = endNode.childNodes[endNode.childNodes.length - 1]
 							}
@@ -311,9 +462,10 @@ Hello`))
 							FFDedupeCompositionEndRef.current = false // Reset
 							return
 						}
-						const data = getData(rootNodeRef.current)
-						const [pos1, pos2] = getPos() // pos1 and pos2 are expected to be the same
-						dispatch.actionInput(data, pos1, pos2)
+						const data = getData(ref.current)
+						const [pos1, pos2] = getPos()
+						const reset = getReset()
+						dispatch.actionInput(data, pos1, pos2, reset)
 					},
 
 					onDrag: e => e.preventDefault(),
@@ -321,14 +473,15 @@ Hello`))
 
 					// ...
 				},
-				state.components,
 			)}
 			<div style={stylex.parse("m-t:24")}>
 				<pre style={{ ...stylex.parse("pre-wrap fs:12 lh:125%"), MozTabSize: 2, tabSize: 2 }}>
 					{JSON.stringify(
 						{
 							...state,
+
 							components: undefined,
+							reactDOM:   undefined,
 						},
 						null,
 						"\t",
