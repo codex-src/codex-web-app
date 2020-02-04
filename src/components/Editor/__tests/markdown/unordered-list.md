a1d0c8569723494cc8e6ab4bf3d999f09e9e230a
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-02-05 06:26:37 +0900

    Organized tests
    Ideally, we want to separate groups of tests out from each other. As is, if we move some tests into another file, Jest actually runes the test asynchronously, which opens two instances of Gecko/Firefox (leads to errors, of course). If this can be fixed (run async test synchronously), then we can do more to organizes test groups.

--- src/components/Editor/__tests/markdown/unordered-list.md
+++ src/components/Editor/__tests/markdown/unordered-list.md
@@ -1,7 +1,10 @@
+// NOTE: Gecko/Firefox crashses on tabs:
+//
+// Protocol error (Page.dispatchKeyEvent): Component returned failure code: 0xc1f30001 (NS_ERROR_NOT_INITIALIZED) [nsITextInputProcessor.keydown]
+//
 - Text 😀 *emphasis* **strong** `code` [anchor](href) ~strikethrough~.
-	- Text 😀 *emphasis* **strong** `code` [anchor](href) ~strikethrough~.
-		- Text 😀 *emphasis* **strong** `code` [anchor](href) ~strikethrough~.
-			- Text 😀 *emphasis* **strong** `code` [anchor](href) ~strikethrough~.
-		- Text 😀 *emphasis* **strong** `code` [anchor](href) ~strikethrough~.
-	- Text 😀 *emphasis* **strong** `code` [anchor](href) ~strikethrough~.
+- Text 😀 *emphasis* **strong** `code` [anchor](href) ~strikethrough~.
+- Text 😀 *emphasis* **strong** `code` [anchor](href) ~strikethrough~.
+- Text 😀 *emphasis* **strong** `code` [anchor](href) ~strikethrough~.
+- Text 😀 *emphasis* **strong** `code` [anchor](href) ~strikethrough~.
 - Text 😀 *emphasis* **strong** `code` [anchor](href) ~strikethrough~.
--- src/components/Editor/__tests/puppeteer.test.js
+++ src/components/Editor/__tests/puppeteer.test.js
@@ -31,6 +31,16 @@ const sectionBreak    = fs.readFileSync("./src/components/Editor/__tests/markdow
 let page = null
 let exit = null
 
+// TODO:
+//
+// - Backspace and delete rune (incl. emojis)
+// - Backspace and delete word
+// - Backspace and delete line
+// - IME
+// - Undo?
+// - Redo?
+// - readme.md
+//
 beforeAll(async () => {
 	jest.setTimeout(60e3)
 	let browser = null
@@ -51,6 +61,99 @@ afterAll(async () => {
 	await exit()
 })
 
+/*
+ * Type
+ */
+test("can type hello, world (1 of 2)", async () => {
+	await reset(page)
+	await type(page, "Hello, world! 😀")
+	let data = await innerText(page)
+	expect(data).toBe("Hello, world! 😀")
+	for (let index = 0; index < 15; index++) {
+		await press(page, "Backspace")
+	}
+	await press(page, "Backspace")
+	data = await innerText(page)
+	expect(data).toBe("")
+})
+
+test("can type hello, world (2 of 2)", async () => {
+	await reset(page)
+	await type(page, "Hello, world! 😀")
+	let data = await innerText(page)
+	expect(data).toBe("Hello, world! 😀")
+	for (let index = 0; index < 15; index++) {
+		await press(page, "ArrowLeft")
+	}
+	for (let index = 0; index < 15; index++) {
+		await press(page, "Delete")
+	}
+	await press(page, "Delete")
+	data = await innerText(page)
+	expect(data).toBe("")
+})
+
+test("can type multiline hello, world! (1 of 2)", async () => {
+	await reset(page)
+	await type(page, "Hello, world! 😀\n\nHello, world! 😀\n\nHello, world! 😀")
+	let data = await innerText(page)
+	expect(data).toBe("Hello, world! 😀\n\nHello, world! 😀\n\nHello, world! 😀")
+	for (let index = 0; index < 49; index++) {
+		await press(page, "Backspace")
+	}
+	await press(page, "Backspace")
+	data = await innerText(page)
+	expect(data).toBe("")
+})
+
+test("can type multiline hello, world! (2 of 2)", async () => {
+	await reset(page)
+	await type(page, "Hello, world! 😀\n\nHello, world! 😀\n\nHello, world! 😀")
+	let data = await innerText(page)
+	expect(data).toBe("Hello, world! 😀\n\nHello, world! 😀\n\nHello, world! 😀")
+	for (let index = 0; index < 49; index++) {
+		await press(page, "ArrowLeft")
+	}
+	for (let index = 0; index < 49; index++) {
+		await press(page, "Delete")
+	}
+	await press(page, "Delete")
+	data = await innerText(page)
+	expect(data).toBe("")
+})
+
+test("can type enter (1 of 2)", async () => {
+	await reset(page)
+	await type(page, "\n".repeat(100))
+	let data = await innerText(page)
+	expect(data).toBe("\n".repeat(100))
+	for (let index = 0; index < 100; index++) {
+		await press(page, "Backspace")
+	}
+	await press(page, "Backspace")
+	data = await innerText(page)
+	expect(data).toBe("")
+})
+
+test("can type enter (2 of 2)", async () => {
+	await reset(page)
+	await type(page, "\n".repeat(100))
+	let data = await innerText(page)
+	expect(data).toBe("\n".repeat(100))
+	for (let index = 0; index < 100; index++) {
+		await press(page, "ArrowLeft")
+	}
+	for (let index = 0; index < 100; index++) {
+		await press(page, "Delete")
+	}
+	await press(page, "Delete")
+	data = await innerText(page)
+	expect(data).toBe("")
+})
+
+/*
+ * Markdown
+ */
 test("header.md", async () => {
 	await reset(page)
 	await type(page, header)
