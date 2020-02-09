7e40243f401eee98cf0eb7cb4046ddb4319a02a9
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-02-10 03:48:32 +0900

    Tweaked getStatusStringLHS implementation

--- src/components/Editor/StatusBars/getStatusString.js
+++ src/components/Editor/StatusBars/getStatusString.js
@@ -6,25 +6,24 @@ function formatCount({ count, desc }) {
 	return `${formatComma({ count })} ${desc}${count === 1 ? "" : "s"}`
 }
 
-// Gets the status string for the LHS.
-//
-// TODO: Add tests
+// Gets the status string for the LHS:
 export function getStatusStringLHS(state, { line, column, selectedLines, selectedCharacters }) {
 	if (!state.isFocused) {
 		return "No selection"
 	}
-	if (selectedCharacters.count) {
+	if (state.hasSelection) {
 		if (selectedLines.count < 2) {
-			return `Selected ${formatCount(selectedCharacters)}`
-		}
-		return `Selected ${formatCount(selectedLines)}, ${formatCount(selectedCharacters)}`
+			return `${formatCount(selectedCharacters)} selected`
+		} // else if (state.pos1.pos || state.pos2.pos < state.data.length) {
+		// 	return `${formatCount(selectedLines)}, ${formatCount(selectedCharacters)} selected`
+		// }
+		// return "Document selected"
+		return `${formatCount(selectedLines)}, ${formatCount(selectedCharacters)} selected`
 	}
 	return `Line ${formatComma(line)}, column ${formatComma(column)}`
 }
 
 // Gets the status string for the RHS.
-//
-// TODO: Add tests
 export function getStatusStringRHS(state, { words, duration }) {
 	if (duration.count < 2) {
 		return formatCount(words)
