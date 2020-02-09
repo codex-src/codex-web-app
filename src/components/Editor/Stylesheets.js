1de135c9f54959f81c94818ecd0b5432dbbeef58
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-02-09 19:02:24 +0900

    Fixed small bug related to code font-size

--- src/components/Editor/Stylesheets.js
+++ src/components/Editor/Stylesheets.js
@@ -152,14 +152,14 @@ const TextStylesheet = props => (
 	font: 19px/1.65 system-ui;
 }
 
-.code-block ,
+.code-block,
 .code {
 	-webkit-font-smoothing:  auto;
 	-moz-osx-font-smoothing: auto;
 
 	-moz-tab-size: 2;
 	tab-size: 2;
-	font: 1em/1.4 "iA Writer Mono", monospace;
+	font: calc(16/19 * 1em)/1.4 "iA Writer Mono", monospace;
 }
 
 .header {
