bad77b48e8f88a34f718d9999f3992527ef8a38f
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-02-14 13:00:42 +0900

    Properly integrated PurgeCSS -> 1.93 KB (-132.74 KB)

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 // https://tailwindcss.com/docs/controlling-file-size/#setting-up-purgecss
+// https://github.com/danestves/danestves/blob/master/postcss.config.js
 const purgecss = require("@fullhuman/postcss-purgecss")({
-	content: ["./src/**/*.js"],
+	content: ["./src/**/*.{js}"],
 	defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
 })
 
