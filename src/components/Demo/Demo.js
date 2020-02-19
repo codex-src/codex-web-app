57134851d99727cf53c1c2b34d94c25e4125d796
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-02-20 05:06:42 +0900

    Readme tweaks

--- src/components/Demo/Demo.js
+++ src/components/Demo/Demo.js
@@ -14,87 +14,14 @@ const LOCAL_STORAGE_KEY = "codex-app"
 
 const demo = localStorage.getItem(LOCAL_STORAGE_KEY) || raw("./markdown/demo.md")
 
-// function WithReadme({ readme, setReadme, ...props }) {
-// 	const ref = React.useRef()
-//
-// 	React.useEffect(() => {
-// 		if (!readme) {
-// 			// Off:
-// 			ref.current.classList.remove("readme-editor-active")
-// 		} else {
-// 			// On:
-// 			ref.current.classList.add("readme-editor-active")
-// 		}
-// 	}, [readme])
-//
-// 	return (
-// 		<div>
-// 			<div className="fixed right-0 inset-y-0 z-30">
-// 				<div className="transform scale-90">
-// 					<div ref={ref} className="readme-editor-enter p-6 w-full max-w-md max-h-full bg-white rounded-xl shadow-hero-xl z-30 overflow-y-scroll scrolling-touch">
-// 						<div className="p-6 absolute right-0 top-0">
-// 							<div className="p-2 text-gray-800 hover:bg-md-gray-100 active:bg-md-gray-200 rounded-full cursor-pointer" onClick={e => setReadme(false)}>
-// 								<Feather.X className="p-px w-5 h-5 stroke-500" />
-// 							</div>
-// 						</div>
-// 						<EditorReadme />
-// 					</div>
-// 				</div>
-// 			</div>
-// 			{props.children}
-// 		</div>
-// 	)
-// }
-
-// function WithReadme({ readme, setReadme, ...props }) {
-// 	const ref = React.useRef()
-//
-// 	React.useEffect(() => {
-// 		if (!readme) {
-// 			// Off:
-// 			ref.current.classList.remove("readme-editor-active")
-// 		} else {
-// 			// On:
-// 			ref.current.classList.add("readme-editor-active")
-// 		}
-// 	}, [readme])
-//
-// 	return (
-// 		<div>
-// 			<div
-// 				className="fixed right-0 inset-y-0 w-8 bg-black"
-// 				onPointerEnter={e => setReadme(true)}
-// 				onPointerLeave={e => setReadme(false)}
-// 			>
-// 				<div className="mr-8 absolute right-full inset-y-0 flex flex-row justify-end items-start pointer-events-none" style={{ width: "calc(100vw - 100%)" }}>
-// 					{/* Readme: */}
-// 					<div ref={ref} className="readme-editor-enter p-6 w-full max-w-md max-h-full bg-white rounded-xl shadow-hero-xl z-30 overflow-y-scroll scrolling-touch">
-// 						{/* X: */}
-// 						<div className="p-6 absolute right-0 top-0">
-// 							<div className="p-2 text-gray-800 hover:bg-md-gray-100 active:bg-md-gray-200 rounded-full cursor-pointer" onClick={e => setReadme(false)}>
-// 								<Feather.X className="p-px w-5 h-5 stroke-500" />
-// 							</div>
-// 						</div>
-// 						<EditorReadme />
-// 					</div>
-// 				</div>
-// 			</div>
-// 			{props.children}
-// 		</div>
-// 	)
-// }
-
 function WithReadme({ readme, setReadme, ...props }) {
 	const ref = React.useRef()
 	const didPointerEnter = React.useRef()
 
 	React.useEffect(() => {
 		if (!readme) {
-			// Off:
 			ref.current.classList.remove("readme-editor-active")
-			// didPointerEnter.current = false // Reset
 		} else {
-			// On:
 			ref.current.classList.add("readme-editor-active")
 		}
 	}, [readme])
@@ -119,44 +46,31 @@ function WithReadme({ readme, setReadme, ...props }) {
 	return (
 		<div>
 			<div
-				className="fixed right-0 inset-y-0 w-12"
+				className="p-6 fixed right-0 inset-y-0 transform translate-x-full z-30"
 				onPointerEnter={handlePointerEnter}
 				onPointerLeave={handlePointerLeave}
 			>
-				<div className="absolute left-full inset-y-0">
-					{/* Readme: */}
-					<div
-						ref={ref}
-						className="readme-editor-enter p-6 max-h-full bg-white rounded-xl shadow-hero-xl z-30 overflow-y-scroll scrolling-touch"
-						style={{ width: "28rem" }}
-					>
-						{/* X: */}
-						<div className="p-6 absolute right-0 top-0">
-							<div className="p-2 text-gray-800 hover:bg-md-gray-100 active:bg-md-gray-200 rounded-full cursor-pointer" onClick={e => setReadme(false)}>
-								<Feather.X className="p-px w-5 h-5 stroke-500" />
-							</div>
+				<div className="absolute right-full inset-y-0">
+					<div className="w-12 h-full" />
+				</div>
+				{/* Readme: */}
+				<div
+					ref={ref}
+					className="readme-editor-enter p-6 w-full max-w-md max-h-full bg-white rounded-xl shadow-hero-xl z-30 overflow-y-scroll scrolling-touch"
+				>
+					{/* X: */}
+					<div className="p-6 absolute right-0 top-0">
+						<div className="p-2 text-gray-800 hover:bg-md-gray-100 active:bg-md-gray-200 rounded-full cursor-pointer" onClick={e => setReadme(false)}>
+							<Feather.X className="p-px w-5 h-5 stroke-500" />
 						</div>
-						<EditorReadme />
 					</div>
+					<EditorReadme />
 				</div>
 			</div>
 			{props.children}
 		</div>
 	)
 }
-
-// <div className="absolute right-full inset-y-0 flex flex-row justify-end items-start pointer-events-none" style={{ width: "calc(100vw - 100%)" }}>
-// 	{/* Readme: */}
-// 	<div ref={ref} className="readme-editor-enter p-6 w-full max-w-md max-h-full bg-white rounded-xl shadow-hero-xl z-30 overflow-y-scroll scrolling-touch">
-// 		{/* X: */}
-// 		<div className="p-6 absolute right-0 top-0">
-// 			<div className="p-2 text-gray-800 hover:bg-md-gray-100 active:bg-md-gray-200 rounded-full cursor-pointer" onClick={e => setReadme(false)}>
-// 				<Feather.X className="p-px w-5 h-5 stroke-500" />
-// 			</div>
-// 		</div>
-// 		<EditorReadme />
-// 	</div>
-// </div>
 
 function Demo(props) {
 	const editor = Editor.useEditor(demo, { shortcuts: true /* statusBar: true */ })
