43e331a9998daf8526994ff263b26dbf544c591e
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-02-25 06:47:20 +0900

    Small changes

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -6,6 +6,7 @@ import { ReactComponent as GitHubLogo } from "svg/github.svg"
 import { ReactComponent as GoogleLogo } from "svg/google.svg"
 
 const SignInWithGitHub = props => (
+	// Based on Nav CTA:
 	<div className="px-4 py-3 flex flex-row justify-center items-center w-full bg-gray-900 hover:bg-gray-800 rounded-lg shadow-hero hover:shadow-hero-md cursor-pointer select-none tx-150">
 		<div className="mr-3">
 			<GitHubLogo className="w-6 h-6 text-white" />
@@ -17,6 +18,7 @@ const SignInWithGitHub = props => (
 )
 
 const SignInWithGoogle = props => (
+	// Based on Nav CTA:
 	<div className="px-4 py-3 flex flex-row justify-center items-center w-full bg-white hover:bg-gray-100 rounded-lg shadow-hero hover:shadow-hero-md cursor-pointer select-none tx-150">
 		<div className="mr-3">
 			<GoogleLogo className="w-6 h-6" />
@@ -31,7 +33,7 @@ const Auth = props => (
 	<div className="p-6 py-64 flex flex-row justify-center">
 		<div className="flex flex-col items-center w-64">
 
-			{/* Logo: */}
+			{/* Logo; based on Nav LHS: */}
 			<div className="flex flex-row items-center transform scale-110">
 				<div className="mr-3 text-md-blue-a400">
 					<Feather.Layers className="w-6 h-6" />
