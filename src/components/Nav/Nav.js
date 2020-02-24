806f9749cd9bfdde4c5aca98215f593dc45f3ff7
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-02-24 18:56:22 +0900

    Small tweaks

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,19 @@
 import * as Feather from "react-feather"
 import CSSDebugger from "utils/CSSDebugger"
-import RouterLink from "utils/RouterLink"
 import React from "react"
+import RouterLink from "utils/RouterLink"
 import { ReactComponent as CodexLogo } from "./codex.svg"
 
 const Link = ({ className, ...props }) => (
 	<RouterLink className={`${className || ""} select-none`} {...props} />
+)
+
+const NavLink = ({ text, ...props }) => (
+	<Link className="px-3 flex flex-row items-center text-gray-700 hover:text-md-blue-a400 trans-75" {...props}>
+		<p className="font-500">
+			{text}
+		</p>
+	</Link>
 )
 
 const Nav = props => (
@@ -14,35 +22,31 @@ const Nav = props => (
 
 			{/* LHS: */}
 			<div className="-mx-3 flex flex-row">
-				<Link className="px-3 flex flex-row items-center hover:text-md-blue-a400" to="/" data-e2e="nav-home">
+				<Link className="px-3 flex flex-row items-center hover:text-md-blue-a400 trans-75" to="/" data-e2e="nav-home">
 					<CodexLogo style={{ width: 60 * 1.5, height: 60 }} />
 				</Link>
 			</div>
 
 			{/* RHS: */}
 			<div className="-mx-3 flex flex-row">
-				<Link className="px-3 flex flex-row items-center text-gray-700 hover:text-md-blue-a400" to="TODO">
-					<p className="font-500">
-						What is Codex?
-					</p>
-				</Link>
-				<Link className="px-3 flex flex-row items-center text-gray-700 hover:text-md-blue-a400" to="TODO">
-					<p className="font-500">
-						Features
-					</p>
-				</Link>
-				<Link className="px-3 flex flex-row items-center text-gray-700 hover:text-md-blue-a400" to="TODO">
-					<p className="font-500">
-						Demo
-					</p>
-				</Link>
-				<Link className="px-3 flex flex-row items-center text-gray-700 hover:text-md-blue-a400" to="TODO">
-					<p className="font-500">
-						Pricing
-					</p>
-				</Link>
+				<NavLink
+					to="TODO"
+					text="What is Codex?"
+				/>
+				<NavLink
+					to="TODO"
+					text="Features"
+				/>
+				<NavLink
+					to="TODO"
+					text="Demo"
+				/>
+				<NavLink
+					to="TODO"
+					text="Pricing"
+				/>
 				<div className="mx-3 flex flex-row items-center">
-					<Link className="p-3 flex flex-row items-center text-md-blue-a400 hover:bg-gray-100 active:bg-gray-200 rounded-md shadow-hero">
+					<Link className="p-3 flex flex-row items-center text-md-blue-a400 hover:bg-gray-100 active:bg-gray-200 rounded-md shadow-hero hover:shadow-hero-md trans-150">
 						<Feather.FileText className="mr-3 stroke-500 w-4 h-4" />
 						<p className="font-500">
 							Open your Codex
