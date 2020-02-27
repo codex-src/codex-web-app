e1861cba2664666bf8395711177e06596d395ce9
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-02-28 06:21:01 +0900

    Clean up App implementation

--- src/__constants/constants.js
+++ src/__constants/constants.js
@@ -3,6 +3,7 @@ export const PATH_AUTH = "/continue"
 export const PATH_DEMO = "/demo"
 export const PATH_HELP = "/support"
 export const PATH_HOME = "/"
+export const PATH_LOST = "/404"
 export const PATH_TODO = "/TODO"
 export const URL       = process.env.PUBLIC_URL || `http://localhost:${process.env.PORT || "3000"}`
 export const URL_REPO  = "https://github.com/codex-src/codex-app"
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,93 +8,49 @@ import Demo from "components/Demo"
 import Home from "components/Home"
 import React from "react"
 
-const App = props => (
+const Providers = props => (
 	<Router.BrowserRouter>
 		<User.Provider>
 			<ProgressBar.Provider>
-				<ProgressBar.ProgressBar />
-				<Router.Switch>
-
-					{/* <User.UnprotectedRoute */}
-					{/* 	path="/" */}
-					{/* 	exact */}
-					{/* 	title="" */}
-					{/* 	children={<Home />} */}
-					{/* /> */}
-
-					<User.UnprotectedRoute
-						path={constants.PATH_AUTH}
-						title="Open your Codex"
-						children={<Auth />}
-					/>
-					<User.UnprotectedRoute
-						path={constants.PATH_DEMO}
-						exact
-						title="Demo"
-						children={<Demo />}
-					/>
-
-					<User.Context.Consumer>
-						{user => !user ? (
-							// Unauthenticated users:
-							<User.UnprotectedRoute path={constants.PATH_HOME} exact>
-								<Home key={random.newFourByteHash()} />
-							</User.UnprotectedRoute>
-						) : (
-							// Authenticated users:
-							<User.ProtectedRoute path={constants.PATH_HOME} exact>
-								<Home key={random.newFourByteHash()} />
-							</User.ProtectedRoute>
-						)}
-					</User.Context.Consumer>
-
-					{/* <User.UnprotectedRoute */}
-					{/* 	path="/our-story" */}
-					{/* 	exact */}
-					{/* 	title="Our story" */}
-					{/* 	component={props => "TODO"} */}
-					{/* /> */}
-					{/* <User.UnprotectedRoute */}
-					{/* 	path="/features" */}
-					{/* 	exact */}
-					{/* 	title="Features" */}
-					{/* 	component={props => "TODO"} */}
-					{/* /> */}
-					{/* <User.UnprotectedRoute */}
-					{/* 	path="/pricing" */}
-					{/* 	exact */}
-					{/* 	title="Pricing" */}
-					{/* 	component={props => "TODO"} */}
-					{/* /> */}
-					{/* <User.UnprotectedRoute */}
-					{/* 	path="/faq" */}
-					{/* 	exact */}
-					{/* 	title="FAQ" */}
-					{/* 	component={props => "TODO"} */}
-					{/* /> */}
-					{/* <User.UnprotectedRoute */}
-					{/* 	path="/sign-up" */}
-					{/* 	exact */}
-					{/* 	title="Sign up" */}
-					{/* 	component={Forms.SignUpFlow} */}
-					{/* /> */}
-					{/* <User.UnprotectedRoute */}
-					{/* 	path="/sign-in" */}
-					{/* 	exact */}
-					{/* 	title="Sign in" */}
-					{/* 	component={Forms.SignIn} */}
-					{/* /> */}
-					{/* <User.UnprotectedRoute */}
-					{/* 	path="/reset-password" */}
-					{/* 	exact */}
-					{/* 	title="Reset password" */}
-					{/* 	component={Forms.ResetPassword} */}
-					{/* /> */}
-
-				</Router.Switch>
+				{props.children}
 			</ProgressBar.Provider>
 		</User.Provider>
 	</Router.BrowserRouter>
+)
+
+const App = props => (
+	<Providers>
+		<ProgressBar.ProgressBar />
+		<Router.Switch>
+
+			<User.UnprotectedRoute
+				path={constants.PATH_AUTH}
+				title="Open your Codex"
+				children={<Auth />}
+			/>
+			<User.UnprotectedRoute
+				path={constants.PATH_DEMO}
+				exact
+				title="Demo"
+				children={<Demo />}
+			/>
+
+			<User.Context.Consumer>
+				{user => !user ? (
+					// Unauthenticated users:
+					<User.UnprotectedRoute path={constants.PATH_HOME} exact>
+						<Home key={random.newFourByteHash()} />
+					</User.UnprotectedRoute>
+				) : (
+					// Authenticated users:
+					<User.ProtectedRoute path={constants.PATH_HOME} exact>
+						<Home key={random.newFourByteHash()} />
+					</User.ProtectedRoute>
+				)}
+			</User.Context.Consumer>
+
+		</Router.Switch>
+	</Providers>
 )
 
 export default App
