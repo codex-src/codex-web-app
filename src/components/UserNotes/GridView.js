5b2593362c4e82408494153905aec5820eab650c
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-03-01 00:48:11 +0900

    Removed duplicate mock editor instances

--- src/components/UserNotes/GridView.js
+++ src/components/UserNotes/GridView.js
@@ -7,87 +7,17 @@ import "./GridView.css"
 
 const GridView = props => (
 	<div className="grid">
-		<div className="grid-item">
-			<Link className="pb-2/3 relative block" to={constants.TODO}>
-				<div className="absolute inset-0">
-					<div className="h-full bg-white rounded-lg shadow-hero overflow-y-scroll scrolling-touch tx-150">
-						<MockEditor />
+		{[...new Array(20)].map((_, index) => (
+			<div key={index} className="grid-item">
+				<Link className="pb-2/3 relative block" to={constants.TODO}>
+					<div className="absolute inset-0">
+						<div className="h-full bg-white rounded-lg shadow-hero overflow-y-scroll scrolling-touch tx-150">
+							<MockEditor />
+						</div>
 					</div>
-				</div>
-			</Link>
-		</div>
-		<div className="grid-item">
-			<Link className="pb-2/3 relative block" to={constants.TODO}>
-				<div className="absolute inset-0">
-					<div className="h-full bg-white rounded-lg shadow-hero overflow-y-scroll scrolling-touch tx-150">
-						<MockEditor />
-					</div>
-				</div>
-			</Link>
-		</div>
-		<div className="grid-item">
-			<Link className="pb-2/3 relative block" to={constants.TODO}>
-				<div className="absolute inset-0">
-					<div className="h-full bg-white rounded-lg shadow-hero overflow-y-scroll scrolling-touch tx-150">
-						<MockEditor />
-					</div>
-				</div>
-			</Link>
-		</div>
-		<div className="grid-item">
-			<Link className="pb-2/3 relative block" to={constants.TODO}>
-				<div className="absolute inset-0">
-					<div className="h-full bg-white rounded-lg shadow-hero overflow-y-scroll scrolling-touch tx-150">
-						<MockEditor />
-					</div>
-				</div>
-			</Link>
-		</div>
-		<div className="grid-item">
-			<Link className="pb-2/3 relative block" to={constants.TODO}>
-				<div className="absolute inset-0">
-					<div className="h-full bg-white rounded-lg shadow-hero overflow-y-scroll scrolling-touch tx-150">
-						<MockEditor />
-					</div>
-				</div>
-			</Link>
-		</div>
-		<div className="grid-item">
-			<Link className="pb-2/3 relative block" to={constants.TODO}>
-				<div className="absolute inset-0">
-					<div className="h-full bg-white rounded-lg shadow-hero overflow-y-scroll scrolling-touch tx-150">
-						<MockEditor />
-					</div>
-				</div>
-			</Link>
-		</div>
-		<div className="grid-item">
-			<Link className="pb-2/3 relative block" to={constants.TODO}>
-				<div className="absolute inset-0">
-					<div className="h-full bg-white rounded-lg shadow-hero overflow-y-scroll scrolling-touch tx-150">
-						<MockEditor />
-					</div>
-				</div>
-			</Link>
-		</div>
-		<div className="grid-item">
-			<Link className="pb-2/3 relative block" to={constants.TODO}>
-				<div className="absolute inset-0">
-					<div className="h-full bg-white rounded-lg shadow-hero overflow-y-scroll scrolling-touch tx-150">
-						<MockEditor />
-					</div>
-				</div>
-			</Link>
-		</div>
-		<div className="grid-item">
-			<Link className="pb-2/3 relative block" to={constants.TODO}>
-				<div className="absolute inset-0">
-					<div className="h-full bg-white rounded-lg shadow-hero overflow-y-scroll scrolling-touch tx-150">
-						<MockEditor />
-					</div>
-				</div>
-			</Link>
-		</div>
+				</Link>
+			</div>
+		))}
 	</div>
 )
 
