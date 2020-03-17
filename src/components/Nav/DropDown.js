2df5fb1021b5f0c0ccfdc615066a294b53b1a957
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-03-18 08:49:54 +0900

    Found a pattern for balancing borders, negative margins and shadow-hero

--- src/components/Nav/DropDown.js
+++ src/components/Nav/DropDown.js
@@ -2,12 +2,12 @@ import $Link from "components/Link"
 import React from "react"
 
 export const HR = props => (
-	<hr className="my-2 border-gray-300 dark:border-gray-700" />
+	<hr className="-mx-px my-2 border-gray-300 dark:border-gray-650" />
 )
 
 export const Link = ({ text, subtext, ...props }) => (
 	// eslint-disable-next-line react/jsx-pascal-case
-	<$Link className="px-4 py-1 text-gray-800 dark:text-gray-200 hover:text-white hover:bg-md-blue-a400" {...props}>
+	<$Link className="-mx-px px-4 py-1 text-gray-800 dark:text-gray-200 hover:text-white hover:bg-md-blue-a400" {...props}>
 		<p className="font-medium -text-px">
 			{text}
 		</p>
@@ -15,11 +15,8 @@ export const Link = ({ text, subtext, ...props }) => (
 )
 
 export const Base = React.forwardRef((props, ref) => (
-	<div ref={ref} className="-mt-2 absolute right-0 top-full w-48 bg-white dark:bg-gray-750 rounded-lg shadow-hero-lg">
-		{/* NOTE (1): Use py-2 because Link uses py-1 */}
-		{/* NOTE (2): py-* needs to be inside */}
-		<div className="py-2 rounded-lg" style={{ boxShadow: "0 0 0 1px var(--gray-700), var(--shadow-md)" }}>
-			{props.children}
-		</div>
+	// NOTE: Use py-2 because Link uses py-1
+	<div ref={ref} className="-mt-2 py-2 absolute right-0 top-full w-48 bg-white dark:bg-gray-750 border border-transparent dark:border-gray-700 rounded-lg shadow-hero-lg">
+		{props.children}
 	</div>
 ))
