3b4a9428aa75205bb4dae2bec17f5710beefe96c
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-03-20 13:00:19 +0900

    UnauthNav is now smooth

--- src/components/Nav/useDarkModeNav.js
+++ src/components/Nav/useDarkModeNav.js
@@ -2,12 +2,21 @@ import React from "react"
 
 // border-b border-transparent dark:border-gray-750
 function useDarkModeNav(ref, darkMode) {
+	// Disable border-color and box-shadow:
+	React.useLayoutEffect(() => {
+		ref.current.style.borderColor = "transparent"
+		ref.current.style.boxShadow = "none"
+	}, [ref])
+
+	// Programmatically enable border-color and box-shadow:
 	React.useLayoutEffect(() => {
 		const handler = e => {
 			if (!window.scrollY) {
-				ref.current.classList.remove(..."border-b border-transparent dark:border-gray-750 shadow".split(" "))
+				ref.current.style.borderColor = "transparent"
+				ref.current.style.boxShadow = "none"
 			} else {
-				ref.current.classList.add(..."border-b border-transparent dark:border-gray-750 shadow".split(" "))
+				ref.current.style.borderColor = ""
+				ref.current.style.boxShadow = ""
 			}
 		}
 		handler()
