c9e97efd14dd5c94fc9e678ae7687ff4f11a0959
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-03-20 10:33:01 +0900

    Added some hover transitions to UnauthNav

--- src/components/Nav/UnauthNav.js
+++ src/components/Nav/UnauthNav.js
@@ -11,8 +11,9 @@ import useDarkModeNav from "./useDarkModeNav"
 import useDropDown from "hooks/useDropDown"
 
 const NavLink = props => (
-	<Link className="px-3 flex flex-row items-center" {...props}>
-		<p className="tracking-wide text-black dark:text-white">
+	// NOTE: Use hover:text-md-blue-a200 for light mode
+	<Link className="px-3 text-black dark:text-white hover:text-md-blue-a400 hover:dark:text-md-blue-a200 flex flex-row items-center transition duration-75" {...props}>
+		<p className="tracking-wide">
 			{props.children}
 		</p>
 	</Link>
