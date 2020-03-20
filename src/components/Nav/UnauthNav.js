cb695ce217241a5d56f811b0432f7bb5762ca5d6
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-03-20 12:13:33 +0900

    Better transition for UnauthNav -- dark mode **does not** have a smooth transition because of border-color?

--- src/components/Nav/UnauthNav.js
+++ src/components/Nav/UnauthNav.js
@@ -138,7 +138,7 @@ const UnauthNav = props => {
 	useDarkModeNav(ref, darkMode)
 
 	return (
-		<div ref={ref} className="fixed inset-x-0 top-0 flex flex-row justify-center bg-white dark:bg-gray-900 z-30">
+		<div ref={ref} className="fixed inset-x-0 top-0 flex flex-row justify-center bg-white dark:bg-gray-900 z-30 transition duration-300">
 			<div className="px-6 flex flex-row justify-between w-full max-w-screen-lg h-20">
 				<Nav />
 			</div>
--- src/components/Nav/useDarkModeNav.js
+++ src/components/Nav/useDarkModeNav.js
@@ -2,20 +2,20 @@ import React from "react"
 
 // border-b border-transparent dark:border-gray-750
 function useDarkModeNav(ref, darkMode) {
-	// React.useLayoutEffect(() => {
-	// 	const handler = e => {
-	// 		if (!window.scrollY) {
-	// 			ref.current.classList.remove("border-b border-transparent dark:border-gray-750 shadow".split(" "))
-	// 		} else {
-	// 			ref.current.classList.add("border-b border-transparent dark:border-gray-750 shadow".split(" "))
-	// 		}
-	// 	}
-	// 	handler()
-	// 	window.addEventListener("scroll", handler, false)
-	// 	return () => {
-	// 		window.removeEventListener("scroll", handler, false)
-	// 	}
-	// }, [ref, darkMode])
+	React.useLayoutEffect(() => {
+		const handler = e => {
+			if (!window.scrollY) {
+				ref.current.classList.remove(..."border-b border-transparent dark:border-gray-750 shadow".split(" "))
+			} else {
+				ref.current.classList.add(..."border-b border-transparent dark:border-gray-750 shadow".split(" "))
+			}
+		}
+		handler()
+		window.addEventListener("scroll", handler, false)
+		return () => {
+			window.removeEventListener("scroll", handler, false)
+		}
+	}, [ref, darkMode])
 }
 
 export default useDarkModeNav
