3972715d44f07cb5ffc1082ef2945c1a7d49e4fc
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-05-15 18:36:32 +0900

    Made <Footer> responsive

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,6 @@
 import AppContainer from "components/AppContainer"
+import ExternalLink from "lib/ExternalLink"
 import React from "react"
-import paths from "paths"
-import { Link } from "react-router-dom"
-import { LinkText } from "components/Styled"
 
 import {
 	GitHubLogo,
@@ -10,30 +8,39 @@ import {
 } from "svgs"
 
 const Footer = () => (
-	// NOTE: border-gray-* matches <RaisedSurfaceSeparator>
-	<div className="py-8 flex flex-col items-center border-t border-gray-300 dark:border-gray-700">
+	<div className="py-4 border-t border-gray-300 dark:border-gray-700">
 		<AppContainer>
-			<div className="flex flex-col items-center">
-
-				{/* Icons */}
-				<div className="flex flex-row items-center">
-					<Link to={paths.home} className="p-4 text-gray-500 hover:text-md-blue-a400 dark:hover:text-md-blue-a200 transition ease-out duration-150">
-						<GitHubLogo
-							className="w-6 h-6"
-						/>
-					</Link>
-					<Link to={paths.home} className="p-4 text-gray-500 hover:text-md-blue-a400 dark:hover:text-md-blue-a200 transition ease-out duration-150">
-						<TwitterLogo
-							className="w-6 h-6"
-						/>
-					</Link>
-				</div>
+			<div className="flex flex-col sm:flex-row justify-between items-center">
 
 				{/* Copyright */}
-				<div className="h-4" />
-				<p className="-text-px tracking-px text-gray-500">
-					© 2020 Codex, Inc. All rights reserved.
-				</p>
+				<div className="order-2 sm:order-1">
+					<p className="flex flex-row items-center tracking-px text-gray-500" style={{ height: "3.5rem" /* h-14 */ }}>
+						© 2020 Codex, Inc. All rights reserved.
+					</p>
+				</div>
+
+				{/* Icons */}
+				<div className="order-1 sm:order-2">
+					<div className="flex flex-row items-center">
+
+						{/* GitHub */}
+						<ExternalLink
+							href="https://github.com/codex-src"
+							className="p-4 text-gray-500 hover:text-md-blue-a400 dark:hover:text-md-blue-a200 transition ease-out duration-150"
+						>
+							<GitHubLogo className="w-6 h-6" />
+						</ExternalLink>
+
+						{/* Twitter */}
+						<ExternalLink
+							href="https://twitter.com/username_ZAYDEK"
+							className="p-4 text-gray-500 hover:text-md-blue-a400 dark:hover:text-md-blue-a200 transition ease-out duration-150"
+						>
+							<TwitterLogo className="w-6 h-6" />
+						</ExternalLink>
+
+					</div>
+				</div>
 
 			</div>
 		</AppContainer>
