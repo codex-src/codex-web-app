20e26440a04bc832b6b1426ba6998897d7194377
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-05-27 20:16:25 +0900

    Added <CodexBadge> component

--- src/components/CodexBadge/index.js
+++ src/components/CodexBadge/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 // https://codepen.io/zaydek/pen/rNOZNwZ
-const CodexLogo = () => (
+const CodexBadge = () => (
 	<div className="em-context flex flex-row items-center select-none">
 		<svg
 			className="w-16 h-16 text-md-blue-a400"
@@ -15,19 +15,7 @@ const CodexLogo = () => (
 		>
 			<path d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
 		</svg>
-		<div className="w-2"></div>
-		<div className="-mt-2">
-			<h1
-				className="text-6xl leading-none Poppins lowercase text-black"
-				style={{
-					clipPath: "inset(18.75% 0 0 0)",
-					letterSpacing: "-0.025em",
-				}}
-			>
-				Codex
-			</h1>
-		</div>
 	</div>
 )
 
-export default CodexLogo
+export default CodexBadge
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,13 +1,19 @@
+import CodexBadge from "components/CodexBadge"
 import React from "react"
 
 // https://images.unsplash.com/photo-1552072232-059f1a5d76c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2900&q=80
 const SignUp = () => (
 	<div className="flex flex-row h-full">
-		<div className="w-full bg-gray-100">
+		<div className="hidden md:block w-full bg-gray-100">
 			todo
 		</div>
-		<div className="px-24 py-48" style={{ width: "64rem" }}>
-			<div>hello</div>
+		<div className="px-24 py-48 flex-shrink-0 w-full max-w-xl">
+			<div style={{ fontSize: "75%" }}>
+				<CodexBadge />
+			</div>
+			<h2 className="font-semibold text-3xl md:text-4xl Poppins text-gray-900">
+				Welcome to Codex
+			</h2>
 			<div>hello</div>
 			<div>hello</div>
 			<div>hello</div>
