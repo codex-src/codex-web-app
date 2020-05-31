39d3d755722247554df73ae816039d154ad5f2ed
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-06-01 03:20:33 +0900

    Collocated action buttons

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -88,185 +88,183 @@ const NoteApp = () => (
 				</div>
 
 				<nav className="mt-8">
+
+					<h2 className="font-semibold text-xs tracking-wider leading-6 uppercase truncate text-cool-gray-400">
+						Starred
+						{/* {" "} */}
+						{/* <svg className="ml-1 w-4 h-4 text-cool-gray-300" fill="currentColor" viewBox="0 0 20 20"> */}
+						{/* 	<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" /> */}
+						{/* </svg> */}
+					</h2>
+
 					<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
 						<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
-							<svg className="mr-2 flex-shrink-0 w-5 h-5 text-cool-gray-400" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
-								<path d="M12 4v16m8-8H4" />
+							<svg className="mr-2 flex-shrink-0 w-5 h-5 text-cool-gray-400" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" stroke="currentColor">
+								<path d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z" />
 							</svg>
-							New Note
+							<span className="truncate">JavaScript in 2020</span>
 						</p>
 					</div>
 					<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
 						<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
-							<svg className="mr-2 flex-shrink-0 w-5 h-5 text-cool-gray-400" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
-								<path d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
+							<span className="mr-2 flex-shrink-0 inline-flex flex-row justify-center items-center w-5 h-5">
+								<E>🧠</E>
+							</span>
+							<span className="truncate">Programming isn’t as hard as you think</span>
+						</p>
+					</div>
+
+				</nav>
+
+				<nav className="mt-6">
+
+					<h2 className="font-semibold text-xs tracking-wider leading-6 uppercase truncate text-cool-gray-400">
+						Drafts
+					</h2>
+
+					<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
+						<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
+							<svg className="mr-2 flex-shrink-0 w-5 h-5 text-cool-gray-400" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" stroke="currentColor">
+								<path d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z" />
 							</svg>
-							Open Note from Desktop
+							<span className="truncate">How to build a beautiful blog</span>
 						</p>
 					</div>
 					<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
 						<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
-							<svg className="mr-2 flex-shrink-0 w-5 h-5 text-cool-gray-400" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
-								<path d="M8 17a5 5 0 01-.916-9.916 5.002 5.002 0 019.832 0A5.002 5.002 0 0116 17m-7-5l3-3m0 0l3 3m-3-3v12" />
-							</svg>
-							Import from GitHub
+							<span className="mr-2 flex-shrink-0 inline-flex flex-row justify-center items-center w-5 h-5">
+								<E>🤔</E>
+							</span>
+							<span className="truncate">What I wish I’d known one year ago</span>
 						</p>
 					</div>
+					<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
+						<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
+							<span className="mr-2 flex-shrink-0 inline-flex flex-row justify-center items-center w-5 h-5">
+								<E>🤦‍♀️</E>
+							</span>
+							<span className="truncate">You don’t know what you don’t know</span>
+						</p>
+					</div>
+					<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
+						<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
+							<span className="mr-2 flex-shrink-0 inline-flex flex-row justify-center items-center w-5 h-5">
+								<E>🚀</E>
+							</span>
+							<span className="truncate">What I learned from Carl Sagan</span>
+						</p>
+					</div>
+					<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
+						<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
+							<span className="mr-2 flex-shrink-0 inline-flex flex-row justify-center items-center w-5 h-5">
+								<E>👏</E>
+							</span>
+							<span className="truncate">Why I love StarTalk</span>
+						</p>
+					</div>
+
 				</nav>
 
 				<nav className="mt-6">
-					<div className="mt-4">
 
-						<h2 className="font-semibold text-xs tracking-wider leading-6 uppercase truncate text-cool-gray-400">
-							Starred
-							{/* {" "} */}
-							{/* <svg className="ml-1 w-4 h-4 text-cool-gray-300" fill="currentColor" viewBox="0 0 20 20"> */}
-							{/* 	<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" /> */}
-							{/* </svg> */}
-						</h2>
+					<h2 className="font-semibold text-xs tracking-wider leading-6 uppercase truncate text-cool-gray-400">
+						Shared
+					</h2>
 
-						<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
-							<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
-								<svg className="mr-2 flex-shrink-0 w-5 h-5 text-cool-gray-400" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" stroke="currentColor">
-									<path d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z" />
-								</svg>
-								<span className="truncate">JavaScript in 2020</span>
-							</p>
-						</div>
-						<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
-							<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
-								<span className="mr-2 flex-shrink-0 inline-flex flex-row justify-center items-center w-5 h-5">
-									<E>🧠</E>
-								</span>
-								<span className="truncate">Programming isn’t as hard as you think</span>
-							</p>
-						</div>
-
+					<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
+						<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
+							<svg className="mr-2 flex-shrink-0 w-5 h-5 text-cool-gray-400" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" stroke="currentColor">
+								<path d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z" />
+							</svg>
+							<span className="truncate">Why you should learn programming in 2020</span>
+						</p>
 					</div>
-				</nav>
-
-				<nav className="mt-6">
-					<div className="mt-4">
-
-						<h2 className="font-semibold text-xs tracking-wider leading-6 uppercase truncate text-cool-gray-400">
-							Drafts
-						</h2>
-
-						<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
-							<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
-								<svg className="mr-2 flex-shrink-0 w-5 h-5 text-cool-gray-400" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" stroke="currentColor">
-									<path d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z" />
-								</svg>
-								<span className="truncate">How to build a beautiful blog</span>
-							</p>
-						</div>
-						<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
-							<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
-								<span className="mr-2 flex-shrink-0 inline-flex flex-row justify-center items-center w-5 h-5">
-									<E>🤔</E>
-								</span>
-								<span className="truncate">What I wish I’d known one year ago</span>
-							</p>
-						</div>
-						<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
-							<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
-								<span className="mr-2 flex-shrink-0 inline-flex flex-row justify-center items-center w-5 h-5">
-									<E>🤦‍♀️</E>
-								</span>
-								<span className="truncate">You don’t know what you don’t know</span>
-							</p>
-						</div>
-						<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
-							<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
-								<span className="mr-2 flex-shrink-0 inline-flex flex-row justify-center items-center w-5 h-5">
-									<E>🚀</E>
-								</span>
-								<span className="truncate">What I learned from Carl Sagan</span>
-							</p>
-						</div>
-						<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
-							<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
-								<span className="mr-2 flex-shrink-0 inline-flex flex-row justify-center items-center w-5 h-5">
-									<E>👏</E>
-								</span>
-								<span className="truncate">Why I love StarTalk</span>
-							</p>
-						</div>
-
+					<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
+						<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
+							<span className="mr-2 flex-shrink-0 inline-flex flex-row justify-center items-center w-5 h-5">
+								<E>🤷‍♀️</E>
+							</span>
+							<span className="truncate">Who really knows what programming is?</span>
+						</p>
 					</div>
-				</nav>
-
-				<nav className="mt-6">
-					<div className="mt-4">
-
-						<h2 className="font-semibold text-xs tracking-wider leading-6 uppercase truncate text-cool-gray-400">
-							Shared
-						</h2>
-
-						<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
-							<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
-								<svg className="mr-2 flex-shrink-0 w-5 h-5 text-cool-gray-400" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" stroke="currentColor">
-									<path d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z" />
-								</svg>
-								<span className="truncate">Why you should learn programming in 2020</span>
-							</p>
-						</div>
-						<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
-							<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
-								<span className="mr-2 flex-shrink-0 inline-flex flex-row justify-center items-center w-5 h-5">
-									<E>🤷‍♀️</E>
-								</span>
-								<span className="truncate">Who really knows what programming is?</span>
-							</p>
-						</div>
-						<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
-							<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
-								<span className="mr-2 flex-shrink-0 inline-flex flex-row justify-center items-center w-5 h-5">
-									<E>🎨</E>
-								</span>
-								<span className="truncate">The missing CSS property</span>
-							</p>
-						</div>
-						<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
-							<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
-								<svg className="mr-2 flex-shrink-0 w-5 h-5 text-cool-gray-400" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" stroke="currentColor">
-									<path d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z" />
-								</svg>
-								<span className="truncate">To build or not to build a blog</span>
-							</p>
-						</div>
-						<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
-							<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
-								<span className="mr-2 flex-shrink-0 inline-flex flex-row justify-center items-center w-5 h-5">
-									<E>🥳</E>
-								</span>
-								<span className="truncate">Surprise! You can now fund me on Patreon</span>
-							</p>
-						</div>
-
+					<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
+						<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
+							<span className="mr-2 flex-shrink-0 inline-flex flex-row justify-center items-center w-5 h-5">
+								<E>🎨</E>
+							</span>
+							<span className="truncate">The missing CSS property</span>
+						</p>
 					</div>
+					<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
+						<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
+							<svg className="mr-2 flex-shrink-0 w-5 h-5 text-cool-gray-400" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" stroke="currentColor">
+								<path d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z" />
+							</svg>
+							<span className="truncate">To build or not to build a blog</span>
+						</p>
+					</div>
+					<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
+						<p className="flex flex-row items-center font-medium text-sm leading-5 text-cool-gray-500">
+							<span className="mr-2 flex-shrink-0 inline-flex flex-row justify-center items-center w-5 h-5">
+								<E>🥳</E>
+							</span>
+							<span className="truncate">Surprise! You can now fund me on Patreon</span>
+						</p>
+					</div>
+
 				</nav>
 
 			</div>
 
 			{/* TODO */}
 			<nav className="mt-8">
+
 				<div>
-					<button className="flex flex-row items-center w-full font-medium text-sm leading-5 text-cool-gray-500">
-						<svg className="mr-2 flex-shrink-0 w-5 h-5 text-cool-gray-400" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.75" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-							<path d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
-						</svg>
-						<span className="truncate">Archive</span>
-					</button>
+					<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
+						<button className="flex flex-row items-center w-full font-medium text-sm leading-5 text-cool-gray-500">
+							<svg className="mr-2 flex-shrink-0 w-5 h-5 text-cool-gray-400" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+								<path d="M12 4v16m8-8H4" />
+							</svg>
+							<span className="truncate">New Note</span>
+						</button>
+					</div>
+					<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
+						<button className="flex flex-row items-center w-full font-medium text-sm leading-5 text-cool-gray-500">
+							<svg className="mr-2 flex-shrink-0 w-5 h-5 text-cool-gray-400" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+								<path d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
+							</svg>
+							<span className="truncate">Open Note from Desktop</span>
+						</button>
+					</div>
+					<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
+						<button className="flex flex-row items-center w-full font-medium text-sm leading-5 text-cool-gray-500">
+							<svg className="mr-2 flex-shrink-0 w-5 h-5 text-cool-gray-400" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+								<path d="M8 17a5 5 0 01-.916-9.916 5.002 5.002 0 019.832 0A5.002 5.002 0 0116 17m-7-5l3-3m0 0l3 3m-3-3v12" />
+							</svg>
+							<span className="truncate">Import from GitHub</span>
+						</button>
+					</div>
 				</div>
-				<div className="mt-4">
-					<button className="flex flex-row items-center w-full font-medium text-sm leading-5 text-cool-gray-500">
-						<svg className="mr-2 flex-shrink-0 w-5 h-5 text-cool-gray-400" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.75" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-							<path d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-						</svg>
-						<span className="truncate">Trash</span>
-					</button>
+
+				<div className="mt-6">
+					<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
+						<button className="flex flex-row items-center w-full font-medium text-sm leading-5 text-cool-gray-500">
+							<svg className="mr-2 flex-shrink-0 w-5 h-5 text-cool-gray-400" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.75" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+								<path d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
+							</svg>
+							<span className="truncate">Archive</span>
+						</button>
+					</div>
+					<div className="-mx-4 px-4 py-1.5 hover:bg-cool-gray-200 transition duration-150 ease-in-out">
+						<button className="flex flex-row items-center w-full font-medium text-sm leading-5 text-cool-gray-500">
+							<svg className="mr-2 flex-shrink-0 w-5 h-5 text-cool-gray-400" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.75" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+								<path d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+							</svg>
+							<span className="truncate">Trash</span>
+						</button>
+					</div>
 				</div>
+
 			</nav>
 
 		</div>
