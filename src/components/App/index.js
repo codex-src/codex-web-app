c3d508b92fb3d5553b45e675c96816bdfdbba3c6
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-06-03 00:22:54 +0900

    Removed ‘·Notes’ text

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -179,7 +179,7 @@ const NoteAppFragment = () => {
 									<path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" />
 								</svg>
 								<span className="truncate">
-									Private Notes
+									Private
 								</span>
 							</p>
 						</button>
@@ -265,7 +265,7 @@ const NoteAppFragment = () => {
 									<path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" />
 								</svg>
 								<span className="truncate">
-									Public Notes
+									Public
 								</span>
 							</p>
 						</button>
