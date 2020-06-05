2dc23a0bab6d75ee474e2091a16310e9a7c6ca71
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-06-05 13:09:00 +0900

    Fixed sidebar bug related to width

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -426,20 +426,24 @@ const NoteAppFragment = () => {
 			</TransitionV2>
 
 			{/* RHS */}
+			{/* TODO: Change to CSS so transition can use media
+			queries */}
 			<TransitionV2
 				on={showSidebar}
 				transition="transition duration-500 ease-in-out"
-				from="transform -translate-x-80"
-				to="transform translate-x-0"
+				from="w-full"
+				to="w-full lg:-w-full-80"
 			>
-				<div className="ml-80 px-6 py-24 relative flex-1">
-					<div className="px-4 py-6 absolute top-0 left-0">
-						<button className="group inline-block focus:outline-none" onClick={() => setShowSidebar(!showSidebar)}>
+				<div className="ml-auto px-6 py-24 relative flex-1" style={{ /* transform: "translateZ(0)", */ willChange: "width", transitionProperty: "width" }}>
+					{/* NOTE: Uses paddingTop: 26 to match LHS;
+					py-6 + (py-1 / 2) */}
+					<div className="px-4 py-6 pt-7 fixed top-0 left-0 pointer-events-none" style={{ paddingTop: 26 }}>
+						<button className="group inline-block focus:outline-none pointer-events-auto" onClick={() => setShowSidebar(!showSidebar)}>
 							<TransitionV2
 								on={showSidebar}
 								transition="transition duration-500 ease-in-out"
 								from="opacity-0 lg:opacity-100 transform translate-x-0"
-								to="opacity-0 transform -translate-x-16"
+								to="opacity-0 transform translate-x-64"
 							>
 								<svg className="w-5 h-5 text-cool-gray-300 hover:text-cool-gray-400 focus:text-cool-gray-400 transition duration-150 ease-in-out" fill="currentColor" viewBox="0 0 20 20">
 									<path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
@@ -447,7 +451,320 @@ const NoteAppFragment = () => {
 							</TransitionV2>
 						</button>
 					</div>
-					{/* ... */}
+
+					<div className="mx-auto w-full max-w-3xl">
+						<form>
+							<div>
+								<div>
+									<div>
+										<h3 className="text-lg leading-6 font-medium text-gray-900">
+											Profile
+										</h3>
+										<p className="mt-1 max-w-2xl text-sm leading-5 text-gray-500">
+											This information will be displayed publicly so be careful what you share.
+										</p>
+									</div>
+									<div className="mt-6 sm:mt-5">
+										<div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+											<label htmlFor="username" className="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
+												Username
+											</label>
+											<div className="mt-1 sm:mt-0 sm:col-span-2">
+												<div className="max-w-lg flex rounded-md shadow-sm">
+													<span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 sm:text-sm">
+														workcation.com/
+													</span>
+													<input id="username" className="flex-1 form-input block w-full min-w-0 rounded-none rounded-r-md transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+												</div>
+											</div>
+										</div>
+
+										<div className="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+											<label htmlFor="about" className="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
+												About
+											</label>
+											<div className="mt-1 sm:mt-0 sm:col-span-2">
+												<div className="max-w-lg flex rounded-md shadow-sm">
+													<textarea id="about" rows="3" className="form-textarea block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"></textarea>
+												</div>
+												<p className="mt-2 text-sm text-gray-500">Write a few sentences about yourself.
+												</p>
+											</div>
+										</div>
+
+										<div className="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-center sm:border-t sm:border-gray-200 sm:pt-5">
+											<label htmlFor="photo" className="block text-sm leading-5 font-medium text-gray-700">
+												Photo
+											</label>
+											<div className="mt-2 sm:mt-0 sm:col-span-2">
+												<div className="flex items-center">
+													<span className="h-12 w-12 rounded-full overflow-hidden bg-gray-100">
+														<svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
+															<path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+														</svg>
+													</span>
+													<span className="ml-5 rounded-md shadow-sm">
+														<button type="button" className="py-2 px-3 border border-gray-300 rounded-md text-sm leading-4 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out">
+															Change
+														</button>
+													</span>
+												</div>
+											</div>
+										</div>
+
+										<div className="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+											<label htmlFor="cover_photo" className="block text-sm leading-5 font-medium text-gray-700 sm:mt-px sm:pt-2">
+												Cover photo
+											</label>
+											<div className="mt-2 sm:mt-0 sm:col-span-2">
+												<div className="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+													<div className="text-center">
+														<svg className="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48">
+															<path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+														</svg>
+														<p className="mt-1 text-sm text-gray-600">
+															<button type="button" className="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition duration-150 ease-in-out">
+																Upload a file
+															</button>
+															or drag and drop
+														</p>
+														<p className="mt-1 text-xs text-gray-500">
+															PNG, JPG, GIF up to 10MB
+														</p>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div className="mt-8 border-t border-gray-200 pt-8 sm:mt-5 sm:pt-10">
+									<div>
+										<h3 className="text-lg leading-6 font-medium text-gray-900">
+											Personal Information
+										</h3>
+										<p className="mt-1 max-w-2xl text-sm leading-5 text-gray-500">
+											Use a permanent address where you can receive mail.
+										</p>
+									</div>
+									<div className="mt-6 sm:mt-5">
+										<div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+											<label htmlFor="first_name" className="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
+												First name
+											</label>
+											<div className="mt-1 sm:mt-0 sm:col-span-2">
+												<div className="max-w-lg rounded-md shadow-sm sm:max-w-xs">
+													<input id="first_name" className="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+												</div>
+											</div>
+										</div>
+
+										<div className="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+											<label htmlFor="last_name" className="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
+												Last name
+											</label>
+											<div className="mt-1 sm:mt-0 sm:col-span-2">
+												<div className="max-w-lg rounded-md shadow-sm sm:max-w-xs">
+													<input id="last_name" className="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+												</div>
+											</div>
+										</div>
+
+										<div className="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+											<label htmlFor="email" className="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
+												Email address
+											</label>
+											<div className="mt-1 sm:mt-0 sm:col-span-2">
+												<div className="max-w-lg rounded-md shadow-sm">
+													<input id="email" type="email" className="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+												</div>
+											</div>
+										</div>
+
+										<div className="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+											<label htmlFor="country" className="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
+												Country / Region
+											</label>
+											<div className="mt-1 sm:mt-0 sm:col-span-2">
+												<div className="max-w-lg rounded-md shadow-sm sm:max-w-xs">
+													<select id="country" className="block form-select w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5">
+														<option>United States
+														</option>
+														<option>Canada
+														</option>
+														<option>Mexico
+														</option>
+													</select>
+												</div>
+											</div>
+										</div>
+
+										<div className="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+											<label htmlFor="street_address" className="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
+												Street address
+											</label>
+											<div className="mt-1 sm:mt-0 sm:col-span-2">
+												<div className="max-w-lg rounded-md shadow-sm">
+													<input id="street_address" className="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+												</div>
+											</div>
+										</div>
+
+										<div className="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+											<label htmlFor="city" className="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
+												City
+											</label>
+											<div className="mt-1 sm:mt-0 sm:col-span-2">
+												<div className="max-w-lg rounded-md shadow-sm sm:max-w-xs">
+													<input id="city" className="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+												</div>
+											</div>
+										</div>
+
+										<div className="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+											<label htmlFor="state" className="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
+												State / Province
+											</label>
+											<div className="mt-1 sm:mt-0 sm:col-span-2">
+												<div className="max-w-lg rounded-md shadow-sm sm:max-w-xs">
+													<input id="state" className="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+												</div>
+											</div>
+										</div>
+
+										<div className="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+											<label htmlFor="zip" className="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
+												ZIP / Postal
+											</label>
+											<div className="mt-1 sm:mt-0 sm:col-span-2">
+												<div className="max-w-lg rounded-md shadow-sm sm:max-w-xs">
+													<input id="zip" className="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div className="mt-8 border-t border-gray-200 pt-8 sm:mt-5 sm:pt-10">
+									<div>
+										<h3 className="text-lg leading-6 font-medium text-gray-900">
+											Notifications
+										</h3>
+										<p className="mt-1 max-w-2xl text-sm leading-5 text-gray-500">
+											We'll always let you know about important changes, but you pick what else you want to hear about.
+										</p>
+									</div>
+									<div className="mt-6 sm:mt-5">
+										<div className="sm:border-t sm:border-gray-200 sm:pt-5">
+											<div role="group" aria-labelledby="label-email">
+												<div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-baseline">
+													<div>
+														<div className="text-base leading-6 font-medium text-gray-900 sm:text-sm sm:leading-5 sm:text-gray-700" id="label-email">
+															By Email
+														</div>
+													</div>
+													<div className="mt-4 sm:mt-0 sm:col-span-2">
+														<div className="max-w-lg">
+															<div className="relative flex items-start">
+																<div className="absolute flex items-center h-5">
+																	<input id="comments" type="checkbox" className="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" />
+																</div>
+																<div className="pl-7 text-sm leading-5">
+																	<label htmlFor="comments" className="font-medium text-gray-700">Comments
+																	</label>
+																	<p className="text-gray-500">Get notified when someones posts a comment on a posting.
+																	</p>
+																</div>
+															</div>
+															<div className="mt-4">
+																<div className="relative flex items-start">
+																	<div className="absolute flex items-center h-5">
+																		<input id="candidates" type="checkbox" className="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" />
+																	</div>
+																	<div className="pl-7 text-sm leading-5">
+																		<label htmlFor="candidates" className="font-medium text-gray-700">Candidates
+																		</label>
+																		<p className="text-gray-500">Get notified when a candidate applies for a job.
+																		</p>
+																	</div>
+																</div>
+															</div>
+															<div className="mt-4">
+																<div className="relative flex items-start">
+																	<div className="absolute flex items-center h-5">
+																		<input id="offers" type="checkbox" className="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" />
+																	</div>
+																	<div className="pl-7 text-sm leading-5">
+																		<label htmlFor="offers" className="font-medium text-gray-700">Offers
+																		</label>
+																		<p className="text-gray-500">Get notified when a candidate accepts or rejects an offer.
+																		</p>
+																	</div>
+																</div>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+										<div className="mt-6 sm:mt-5 sm:border-t sm:border-gray-200 sm:pt-5">
+											<div role="group" aria-labelledby="label-notifications">
+												<div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-baseline">
+													<div>
+														<div className="text-base leading-6 font-medium text-gray-900 sm:text-sm sm:leading-5 sm:text-gray-700" id="label-notifications">
+															Push Notifications
+														</div>
+													</div>
+													<div className="sm:col-span-2">
+														<div className="max-w-lg">
+															<p className="text-sm leading-5 text-gray-500">These are delivered via SMS to your mobile phone.
+															</p>
+															<div className="mt-4">
+																<div className="flex items-center">
+																	<input id="push_everything" name="form-input push_notifications" type="radio" className="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" />
+																	<label htmlFor="push_everything" className="ml-3">
+																		<span className="block text-sm leading-5 font-medium text-gray-700">Everything
+																		</span>
+																	</label>
+																</div>
+																<div className="mt-4 flex items-center">
+																	<input id="push_email" name="form-input push_notifications" type="radio" className="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" />
+																	<label htmlFor="push_email" className="ml-3">
+																		<span className="block text-sm leading-5 font-medium text-gray-700">Same as email
+																		</span>
+																	</label>
+																</div>
+																<div className="mt-4 flex items-center">
+																	<input id="push_nothing" name="form-input push_notifications" type="radio" className="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" />
+																	<label htmlFor="push_nothing" className="ml-3">
+																		<span className="block text-sm leading-5 font-medium text-gray-700">No push notifications
+																		</span>
+																	</label>
+																</div>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="mt-8 border-t border-gray-200 pt-5">
+								<div className="flex justify-end">
+									<span className="inline-flex rounded-md shadow-sm">
+										<button type="button" className="py-2 px-4 border border-gray-300 rounded-md text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out">
+											Cancel
+										</button>
+									</span>
+									<span className="ml-3 inline-flex rounded-md shadow-sm">
+										<button type="submit" className="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
+											Save
+										</button>
+									</span>
+								</div>
+							</div>
+						</form>
+					</div>
+
 				</div>
 			</TransitionV2>
 
@@ -455,6 +772,7 @@ const NoteAppFragment = () => {
 	)
 }
 
+// TODO: Add account icon under show sidebar icon?
 const App = () => (
 	<BrowserRouter>
 		<Switch>
